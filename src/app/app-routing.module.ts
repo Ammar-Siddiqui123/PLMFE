5852f6533577434bb10d493fb32607e64a6c569e
Affan Younus <khubaib.khan@enterprise64.com>
2022-10-27 15:14:00 +0500

    structure alignment and sidenav fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
         component: MainComponent,
       },
       {
-        path: 'all-employees',
+        path: 'employees',
         component: EmployeesComponent,
       }
     ]
