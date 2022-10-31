a29e679acabf1744092e39fa6fa65ebc67fc34f1
Affan <affan.younus@enterprise64.com>
2022-10-31 10:06:31 +0500

    search zone added in employees page

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -35,6 +35,7 @@ import { MainComponent } from './main/main.component';
     CommonModule,
     RouterModule,
     FormsModule,
+    ReactiveFormsModule,
     MatButtonModule,
     MatInputModule,
     MatSidenavModule,
--- src/app/dashboard/employees/employees.component.ts
+++ src/app/dashboard/employees/employees.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
-
+import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: 'app-employees',
@@ -9,14 +11,30 @@ import {MatInputModule} from '@angular/material/input';
 })
 export class EmployeesComponent implements OnInit {
 
-  constructor() { }
-
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
 
   employees_action: boolean = false;
-  employee_zone: string = '';
-  employee_zones_list: string[] = ['Zone A', 'Zone B', 'Zone C', 'Zone D', 'Zone E'];
+  employee_fetched_zones: string[] = ['Zone A', 'Zone B', 'Zone C', 'Zone D', 'Zone E'];
+
+  constructor() { }
 
   ngOnInit(): void {
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
   }
+
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
 
 }
