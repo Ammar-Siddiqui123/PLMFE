2617bd98ed8645f8554b16fee2b746db9891748c
Affan <affan.younus@enterprise64.com>
2022-11-02 13:03:59 +0500

    groups screen is created

--- src/app/dashboard/employees/assigned-functions/assigned-functions.component.ts
+++ src/app/dashboard/employees/assigned-functions/assigned-functions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-assigned-functions',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignedFunctionsComponent implements OnInit {
 
+
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+  employee_fetched_zones: string[] = ['Zone A', 'Zone B', 'Zone C', 'Zone D', 'Zone E'];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
 }
--- src/app/dashboard/employees/unassigned-functions/unassigned-functions.component.ts
+++ src/app/dashboard/employees/unassigned-functions/unassigned-functions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, Observable, startWith } from 'rxjs';
 
 @Component({
   selector: 'app-unassigned-functions',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnassignedFunctionsComponent implements OnInit {
 
+  myControl = new FormControl('');
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+  employee_fetched_zones: string[] = ['Zone A', 'Zone B', 'Zone C', 'Zone D', 'Zone E'];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || '')),
+    );
+
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
   }
 
 }
