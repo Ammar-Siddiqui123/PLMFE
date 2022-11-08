5ef2f090d10d3d128875ecde0dbd843314327ff9
farrukhazeem12 <farrukh.azeem@enterprise64.com>
2022-11-08 14:35:30 +0500

    login and services

---
+++ src/app/Iemployee.ts
@@ -0,0 +1,113 @@
+export interface IEmployee {
+  users?:  string | null | undefined;
+  userName?: string | null | undefined;
+  lastName?:string | null | undefined;
+  wsid?:string | null | undefined;
+  firstName?: string | null | undefined;
+  mi?:string | null | undefined;
+  accessLevel?: string | null | undefined;
+  deleteBy?:string | null | undefined;
+  active?:boolean | null | undefined;
+  maximumOrders?: number | null | undefined;
+  groupName?:string | null | undefined;
+  emailAddress?:string | null | undefined;
+  filter?: any | null | undefined;
+  controlName?:string | null | undefined;
+  newValue?:boolean | null | undefined;
+  startDate?:string | null | undefined;
+  endDate?:string | null | undefined;
+  startRow?:number| null | undefined;
+  draw?:number| null | undefined;
+  access?:string| null | undefined;
+  zone?:string| null | undefined;
+  startLocation?:string| null | undefined;
+  endLocation?:string| null | undefined;
+  oldStartLocation?:string| null | undefined;
+  oldEndLocation?:string| null | undefined;
+  levelID?:string| null | undefined;
+  startShelf?:string| null | undefined;
+  endShelf?:string| null | undefined;
+  group?:string| null | undefined;
+
+
+  }
+
+  export interface Datum {
+    controlName: string;
+    function: string;
+    adminLevel: boolean;
+    }
+
+    
+export interface DataAccessGroup {
+        userName: string;
+        group: string;
+        }
+
+  export interface PickLevel {
+    pickLevel: number;
+    startCarousel: number;
+    endCarousel: number;
+    startShelf: number;
+}
+
+ export interface AllAccess {
+    controlName: string;
+    function: string;
+    adminLevel: boolean;
+}
+
+export interface AllGroup {
+    groupName: string;
+}
+
+export interface ResponseData {
+    allAccess?: AllAccess[];
+    allGroups?: AllGroup[];
+    PickLevel?:PickLevel[];
+    bulkRange?: any[];
+    handledZones?: string[];
+    allZones?: string[];
+    data?: Datum[]
+ 
+
+}
+
+export interface EmployeeObject {
+    responseData: ResponseData|null;
+    responseMessage: string;
+    isExecuted: boolean;
+}
+
+export interface AccessGroupObject {
+    data:DataAccessGroup;
+    responseMessage: string;
+    isExecuted: boolean;
+}
+
+export interface EmployeeResponse {
+  
+    firstName: string | null | undefined;
+    username: string | null | undefined;
+    mi:string | null | undefined;
+    active: boolean;
+    maximumOrders: number;
+    password: string;
+    accessLevel: string | null | undefined;
+    groupName: string | null | undefined;
+    emailAddress: string | null | undefined;
+    wsid?: any;
+  
+    }
+
+export interface AdminEmployeeLookup {
+    employees?: EmployeeResponse[];
+    data?:any;
+}
+
+export interface AdminEmployeeLookupResponse {
+  
+    responseData: AdminEmployeeLookup|null;
+    responseMessage: string;
+    isExecuted: boolean;
+}
---
+++ src/app/employee.service.ts
@@ -0,0 +1,295 @@
+import { Injectable } from '@angular/core';
+import { environment } from "../environments/environment";
+import {
+  HttpClient,
+  HttpEvent,
+  HttpParams,
+  HttpResponse,
+  HttpHeaders
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IEmployee,EmployeeObject,AdminEmployeeLookupResponse,AccessGroupObject, ResponseData,AllGroup,AllAccess } from './Iemployee';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeService {
+
+  constructor(private http: HttpClient) { 
+
+  }
+
+
+  //Test Later
+  public getEmployeeData(employee: IEmployee ): Observable<EmployeeObject> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/GetEmployeeData`, employee,httpOptions);
+  }
+
+
+  public getAdminEmployeeLookup(employee: IEmployee ): Observable<AdminEmployeeLookupResponse> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/EmployeeLookup`, employee,httpOptions);
+  }
+
+  
+
+  public employeeStatsInfo(employee: IEmployee ): Observable<AdminEmployeeLookupResponse> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/EmployeeStatsInfo`, employee,httpOptions);
+  }
+
+
+  public saveAdminEmployee(employee: IEmployee ): Observable<AdminEmployeeLookupResponse> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/SaveNewEmployee`, employee,httpOptions);
+  }
+
+  public deleteAdminEmployee(employee: IEmployee ): Observable<AdminEmployeeLookupResponse> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/DeleteEmployee`, employee,httpOptions);
+  }
+
+
+  public updateAdminEmployee(employee: IEmployee ): Observable<AdminEmployeeLookupResponse> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/UpdateEmployee`, employee,httpOptions);
+  }
+
+  public getAdminEmployeeDetails(employee: IEmployee ): Observable<EmployeeObject> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/GetEmployeeDetails`, employee,httpOptions);
+  }
+  
+  public getControlName(employee: IEmployee ): Observable<EmployeeObject> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/GetControlName`, employee,httpOptions);
+  }
+
+  public updateControlName(employee: IEmployee ): Observable<EmployeeObject> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/UpdateControl`, employee,httpOptions);
+  }
+  
+
+  public deleteControlName(employee: IEmployee ): Observable<EmployeeObject> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/DeleteControl`, employee,httpOptions);
+  }
+
+  public submitControlResponse(employee: IEmployee ): Observable<EmployeeObject> {
+    let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Authorization': 'Basic ' + basicAuth
+      })
+    };
+    return this.http.post<any>(`${environment.apiUrl}/Admin/SubmitControlResponse`, employee,httpOptions);
+  }
+
+//zone
+
+public getZones(employee: IEmployee ): Observable<AdminEmployeeLookupResponse> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/GetZones`, employee,httpOptions);
+}
+
+public updateEmployeeZone(employee: IEmployee ): Observable<AdminEmployeeLookupResponse> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/UpdateEmployeeZone`, employee,httpOptions);
+}
+
+//deleteEmployeeZone
+
+public deleteEmployeeZone(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/DeleteEmployeeZone`, employee,httpOptions);
+}
+
+//insertAllAccess
+public insertAllAccess(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/InsertAllAccess`, employee,httpOptions);
+}
+
+//EmployeeLocation
+public insertEmployeeLocation(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/InsertEmployeeLocation`, employee,httpOptions);
+}
+
+
+
+
+public updateEmployeeLocation(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/UpdateEmployeeLocation`, employee,httpOptions);
+}
+
+
+
+
+public deleteEmployeeLocation(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/DeleteEmployeeLocation`, employee,httpOptions);
+}
+
+
+//picklevels
+
+
+
+
+public insertPickLevels(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/InsertPickLevels`, employee,httpOptions);
+}
+
+public updatePickLevels(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/UpdatePickLevels`, employee,httpOptions);
+}
+
+
+public deletePickLevels(employee: IEmployee ): Observable<EmployeeObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/DeletePickLevels`, employee,httpOptions);
+}
+
+
+//updateAccessGroup
+
+public updateAccessGroup(employee: IEmployee ): Observable<AccessGroupObject> {
+  let basicAuth = window.btoa(`${environment.userName}`+':'+`${environment.password}`)
+  const httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type':  'application/json',
+      'Authorization': 'Basic ' + basicAuth
+    })
+  };
+  return this.http.post<any>(`${environment.apiUrl}/Admin/UpdateAccessGroup`, employee,httpOptions);
+}
+
+
+}
