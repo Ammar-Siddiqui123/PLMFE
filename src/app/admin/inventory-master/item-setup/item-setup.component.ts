0affb6f17f633f05e7314c6b8c7a704392434db9
ovaise64 <ovais.shaikh@enterprise64.com>
2022-12-12 18:19:09 +0500

    PLM-610 Updates
    Changes made on scrolling issue and addition of view button

--- src/app/admin/inventory-master/item-setup/item-setup.component.ts
+++ src/app/admin/inventory-master/item-setup/item-setup.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, TemplateRef, ViewChild,} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-item-setup',
   templateUrl: './item-setup.component.html',
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemSetupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialog: MatDialog) { }
+  @ViewChild('quarantineAction') quarantineTemp: TemplateRef<any>;
 
   ngOnInit(): void {
   }
 
+  quarantineDialog(): void {
+    const dialogRef = this.dialog.open(this.quarantineTemp, {
+      width: '450px'
+    });
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The dialog was closed');
+    });
+  }
 }
