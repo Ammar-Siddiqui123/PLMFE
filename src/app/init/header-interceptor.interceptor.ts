32bafc408592a7c413e90a344a013240fcdfdd61
Ammad Naeem <ammad.naeem@enterprise64.com>
2023-01-10 09:50:32 +0500

    Http token Interceptor fix

--- src/app/init/header-interceptor.interceptor.ts
+++ src/app/init/header-interceptor.interceptor.ts
@@ -14,13 +14,26 @@ export class HeaderInterceptor implements HttpInterceptor {
   constructor() {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    const { _token }  = JSON.parse(localStorage.getItem('user') || '');
-    const authReq = request.clone({
-      headers: new HttpHeaders({
-        'Content-Type':  'application/json',
-        '_token': _token
-      })
-    });
+    let authReq: any;
+    if(localStorage.getItem('user')){
+      const { _token }  = JSON.parse(localStorage.getItem('user') || "{}");
+      authReq = request.clone({
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          '_token': _token
+        })
+      });
+    
+    }
+    else{
+      authReq = request.clone({
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          '_token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJVc2VyTmFtZSI6IjEyMzQiLCJVc2VyVHlwZSI6IlUiLCJBY2Nlc3NEdHRtIjo2MzgwMTk0ODA5MTQ0MTk5NTksIkFwcGxpY2F0aW9uSUQiOiJQaWNrUHJvIiwiQWNjZXNzTGV2ZWwiOiJBZG1pbmlzdHJhdG9yIiwiRmlyc3RMb2dpblRpbWUiOjYzODAxOTQ4MDkxNDQxOTk1OX0.Cyg97oHb_UlFuuBd8MadpE037CW52VPF-_sdYCbxfyA'
+        })
+      });
+    }
     return next.handle(authReq);
+  
   }
 }
