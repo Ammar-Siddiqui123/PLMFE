a7467ce53360137ee1162baf42f218f34b52a581
Ammad Naeem <ammad.naeem@enterprise64.com>
2023-01-11 15:19:09 +0500

    Authorization work.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,16 @@ import { Component, VERSION } from '@angular/core';
 })
 export class AppComponent {
   constructor() {
-    // document.addEventListener('contextmenu', function (e) {
-    //   e.preventDefault();
-    // });
-
-    window.onresize = function () {
-      if ((window.outerHeight - window.innerHeight) > 100)
-        if (confirm('You are not authorized')) {
-          localStorage.clear();
-          window.location.reload()
-        }
-        else {
-          localStorage.clear();
-          window.location.reload()
-        }
-    }
+    // window.onresize = function () {
+    //   if ((window.outerHeight - window.innerHeight) > 100)
+    //     if (confirm('You are not authorized')) {
+    //       localStorage.clear();
+    //       window.location.reload()
+    //     }
+    //     else {
+    //       localStorage.clear();
+    //       window.location.reload()
+    //     }
+    // }
   }
 }
--- src/app/dashboard/side-nav/side-nav.component.ts
+++ src/app/dashboard/side-nav/side-nav.component.ts
@@ -12,31 +12,31 @@ export class SideNavComponent implements OnInit {
   @Input() sideBarOpen: Boolean;
 
   menus: any = [
-    { icon: 'home', title: 'Home', route: '/dashboard' ,permission: ''},
-    { icon: 'electric_bolt', title: 'Import Export', route: '#' ,permission: ''},
+    { icon: 'home', title: 'Home', route: '/dashboard' ,permission: 'Home'},
+    { icon: 'electric_bolt', title: 'Import Export', route: '#' ,permission: 'Import Export'},
     { icon: 'manage_accounts', title: 'Admin', route: '/admin', permission: 'Admin Menu'},
-    { icon: 'checklist', title: 'Induction Manager', route: '#' ,permission: ''},
-    { icon: 'fact_check', title: 'Work Manager', route: '#' ,permission: ''},
-    { icon: 'insert_chart', title: 'Consolidation Manager', route: '#' ,permission: ''},
-    { icon: 'pending_actions', title: 'Order Manager', route: '#' ,permission: ''},
-    { icon: 'schema', title: 'FlowRack Replenish', route: '#',permission: '' }
+    { icon: 'checklist', title: 'Induction Manager', route: '#' ,permission: 'Induction Manager'},
+    { icon: 'fact_check', title: 'Work Manager', route: '#' ,permission: 'Work Manager'},
+    { icon: 'insert_chart', title: 'Consolidation Manager', route: '#' ,permission: 'Consolidation Manager'},
+    { icon: 'pending_actions', title: 'Order Manager', route: '#' ,permission: 'Order Manager'},
+    { icon: 'schema', title: 'FlowRack Replenish', route: '#',permission: 'FlowRack Replenish' }
   ];
   adminMenus: any = [
-    { icon: 'arrow_back', title: 'Admin', route: '/dashboard', class: 'back-class' },
-    { icon: 'assignment_ind', title: 'Employees', route: '/admin/employees' },
-    { icon: 'tune', title: 'Preferences', route: '#' },
-    { icon: 'published_with_changes', title: 'System Replenishment', route: '#' },
-    { icon: 'directions_alt', title: 'Inventory Map', route: '/admin/inventoryMap' },
-    { icon: 'list_alt', title: 'Batch Manager', route: '/admin/batchManager' },
-    { icon: 'analytics', title: 'Reports', route: '#' },
-    { icon: 'my_location', title: 'Location Assignment', route: '/admin/locationAssignment' },
-    { icon: 'low_priority', title: 'Cycle Count', route: '/admin/cycleCounts' },
-    { icon: 'trolley', title: 'Move Items', route: '#' },
-    { icon: 'dvr', title: 'Transactions', route: '#' },
-    { icon: 'ads_click', title: 'Manual Transactions', route: '#' },
-    { icon: 'event_note', title: 'Event Log', route: '#' },
-    { icon: 'airline_stops', title: 'De-Allocate Orders', route: '#' },
-    { icon: 'dashboard', title: 'Inventory', route: '/admin/inventoryMaster' }
+    { icon: 'arrow_back', title: 'Admin', route: '/dashboard', class: 'back-class' ,permission: 'Dashboard'},
+    { icon: 'assignment_ind', title: 'Employees', route: '/admin/employees' ,permission: 'Employees'},
+    { icon: 'tune', title: 'Preferences', route: '#' ,permission: 'Preferences'},
+    { icon: 'published_with_changes', title: 'System Replenishment', route: '#' ,permission: 'Replenishment'},
+    { icon: 'directions_alt', title: 'Inventory Map', route: '/admin/inventoryMap' ,permission: 'Inventory Map'},
+    { icon: 'list_alt', title: 'Batch Manager', route: '/admin/batchManager' ,permission: 'Batch Manager'},
+    { icon: 'analytics', title: 'Reports', route: '#' ,permission: 'Reports'},
+    { icon: 'my_location', title: 'Location Assignment', route: '/admin/locationAssignment' ,permission: 'Location Assignment'},
+    { icon: 'low_priority', title: 'Cycle Count', route: '/admin/cycleCounts' ,permission: 'Cycle Count Manager'},
+    { icon: 'trolley', title: 'Move Items', route: '#' ,permission: 'Move Items'},
+    { icon: 'dvr', title: 'Transactions', route: '#' ,permission: 'Transaction Journal'},
+    { icon: 'ads_click', title: 'Manual Transactions', route: '#' ,permission: 'Manual Transactions'},
+    { icon: 'event_note', title: 'Event Log', route: '#' ,permission: 'Event Log Manager'},
+    { icon: 'airline_stops', title: 'De-Allocate Orders', route: '#' ,permission: 'De-Allocate Orders'},
+    { icon: 'dashboard', title: 'Inventory', route: '/admin/inventoryMaster',permission: 'Inventory' }
   ];
   isParentMenu: boolean = true;
   isChildMenu: boolean = false;
