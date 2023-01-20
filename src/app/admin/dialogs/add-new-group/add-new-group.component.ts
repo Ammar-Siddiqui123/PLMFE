6cb32732789158db124c5a27afcd0c0c46e0186f
uzairsaqib35 <100584002+uzairsaqib786@users.noreply.github.com>
2023-01-20 16:12:59 +0500

    1720 fixed

--- src/app/admin/dialogs/add-new-group/add-new-group.component.ts
+++ src/app/admin/dialogs/add-new-group/add-new-group.component.ts
@@ -44,12 +44,12 @@ export class AddNewGroupComponent implements OnInit {
 
   alphaNumberOnly(string:any) {
     // const regex = "^[a-zA-Z0-9_-]*$";
-    // if(string.match(regex)){
-    //   return true;
-    // }
-    //   return false
-
+    const regex = "^[a-zA-Z0-9_//][a-zA-Z0-9_// ]*[a-zA-Z0-9_//]$";
+    //^[a-zA-Z0-9_][a-zA-Z0-9_ ]*[a-zA-Z0-9_]$
+    if(string.match(regex)){
       return true;
+    }
+      return false;
   }
 
 
