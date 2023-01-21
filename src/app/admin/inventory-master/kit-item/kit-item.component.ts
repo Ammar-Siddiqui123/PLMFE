ce62bec39fa3986d422270869412f6a1caa92017
uzairsaqib35 <100584002+uzairsaqib786@users.noreply.github.com>
2023-01-21 13:31:08 +0500

    1583 resolved
    1583 resolved

--- src/app/admin/inventory-master/kit-item/kit-item.component.ts
+++ src/app/admin/inventory-master/kit-item/kit-item.component.ts
@@ -97,7 +97,7 @@ export class KitItemComponent implements OnInit, OnChanges {
         return;
       }
     });
-      if (e.itemNumber && newRecord) {
+      if (e.itemNumber && newRecord && e.kitQuantity) {
         let paylaod = {
           "itemNumber": this.kitItem.controls['itemNumber'].value,
           "kitItem": newItem,
@@ -122,7 +122,7 @@ export class KitItemComponent implements OnInit, OnChanges {
           }
 
         })
-      } else if (e.itemNumber && !newRecord ){
+      } else if (e.itemNumber && !newRecord && e.kitQuantity){
 
        let paylaod = {
         "itemNumber": this.kitItem.controls['itemNumber'].value,
