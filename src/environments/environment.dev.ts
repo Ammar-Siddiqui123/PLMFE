2d37f34c7918ac6b57169deb81cbcebfe2c125fe
affanyounus-e64 <113903931+affanyounus-e64@users.noreply.github.com>
2023-01-26 23:46:02 +0500

    Update environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://40.77.0.219:9010/api",
+  apiUrl: "https://staging-e64:9010/api",
   userName: "pickpro",
   password: "Admin$11"
 };
