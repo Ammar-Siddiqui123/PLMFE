496a854a3c25eff8907e6285e7a5ee5a47779142
ovaise64 <ovais.shaikh@enterprise64.com>
2023-01-28 18:52:04 +0500

    1885, 1881
    home, workstation screen update with sidenav icons

--- src/app/global-config/workstation/workstation.component.ts
+++ src/app/global-config/workstation/workstation.component.ts
@@ -1,11 +1,61 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
+
+export interface PeriodicElement {
+  position: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 'asdasd'},
+  {position: 'asdasd'},
+  {position: 'asdasd'},
+  {position: 'asdasd'},
+  {position: 'asdasd'},
+  {position: 'asdasd'}
+];
 @Component({
   selector: 'app-workstation',
   templateUrl: './workstation.component.html',
   styleUrls: ['./workstation.component.scss']
 })
 export class WorkstationComponent implements OnInit {
+  displayedColumns: string[] = [ 'position','select','radio'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  selection = new SelectionModel<PeriodicElement>(true, []);
+
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
+  radioLabel(row?: PeriodicElement): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  }
 
   constructor() { }
 
