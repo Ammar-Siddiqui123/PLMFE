1e7942679fe31703ffeca6bd0bc2413af636c64c
Umer Aslam <umer.aslam@enterprise64.com>
2023-01-28 18:48:06 +0500

    PLM-1617 Dashboard connections wip

--- src/app/global-config/database-connections/database-connections.component.ts
+++ src/app/global-config/database-connections/database-connections.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalconfigService } from '../globalconfig.service';
 
 @Component({
   selector: 'app-database-connections',
   templateUrl: './database-connections.component.html',
-  styleUrls: ['./database-connections.component.scss']
+  styleUrls: ['./database-connections.component.scss'],
 })
 export class DatabaseConnectionsComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private globalConfService: GlobalconfigService) {}
+  dbConnectionData=[];
   ngOnInit(): void {
+    this.getMenuData();
   }
 
+  getMenuData() {
+    let payload = {
+      LicenseString:
+        'qdljjBp3O3llQvKEW01qlvO4dTIFf6VMuJvYMgXgEc8U8q+dVlMKt0mKG6qtD9DO',
+      AppUrl: 'CM1',
+      DisplayName: 'Consolidation Manager',
+      AppName: 'Consolidation Manager',
+    };
+    this.globalConfService.get(payload, '/GlobalConfig/Menu').subscribe(
+      (res: any) => {
+        this.dbConnectionData = res && res.data;
+      },
+      (error) => {}
+    );
+  }
 }
