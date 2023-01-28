2b9e1ffc7d7a4568f4e184a4960635c378e1c8dd
Umer Aslam <umer.aslam@enterprise64.com>
2023-01-28 05:54:46 +0500

    PLM-1617-Global-Config setup in progress with ui designs

---
+++ src/app/global-config/dashboard/dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-global-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class GlobalDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/global-config/database-connections/connection-strings/connection-strings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-strings',
+  templateUrl: './connection-strings.component.html',
+  styleUrls: ['./connection-strings.component.scss']
+})
+export class ConnectionStringsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
