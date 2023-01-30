63891285138b58a9e74288562371d75303cf4a22
Umer Aslam <umer.aslam@enterprise64.com>
2023-01-30 10:53:45 +0500

    PLM-1617 Licencing wip

--- src/app/global-config/licensing/licensing.component.ts
+++ src/app/global-config/licensing/licensing.component.ts
@@ -1,31 +1,40 @@
 import { SelectionModel } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
+import { SharedService } from 'src/app/services/shared.service';
+import { GlobalconfigService } from '../globalconfig.service';
 
 export interface PeriodicElement {
   position: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 'asdasd'},
-  {position: 'asdasd'},
-  {position: 'asdasd'},
-  {position: 'asdasd'},
-  {position: 'asdasd'},
-  {position: 'asdasd'},
-  {position: 'asdasd'}
+  { position: 'asdasd' },
+  { position: 'asdasd' },
+  { position: 'asdasd' },
+  { position: 'asdasd' },
+  { position: 'asdasd' },
+  { position: 'asdasd' },
+  { position: 'asdasd' },
 ];
 @Component({
   selector: 'app-licensing',
   templateUrl: './licensing.component.html',
-  styleUrls: ['./licensing.component.scss']
+  styleUrls: ['./licensing.component.scss'],
 })
 export class LicensingComponent implements OnInit {
-  displayedColumns: string[] = [ 'appname','displayname','license','numlicense','status','appurl','save'];
+  displayedColumns: string[] = [
+    'appname',
+    'displayname',
+    'license',
+    'numlicense',
+    'status',
+    'appurl',
+    'save',
+  ];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
-
-
+  licAppData;
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
@@ -48,17 +57,59 @@ export class LicensingComponent implements OnInit {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.position + 1
+    }`;
   }
   radioLabel(row?: PeriodicElement): string {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.position + 1
+    }`;
   }
-  constructor() { }
+  constructor(
+    private globalConfService: GlobalconfigService,
+    private sharedService: SharedService
+  ) {}
 
   ngOnInit(): void {
+    let appData = this.sharedService.getApp();
+    if (!appData) {
+      this.getAppLicense();
+    }else{
+      this.licAppData=appData
+      this.convertToObj();
+    }
+  }
+  async getAppLicense() {
+    // get can access
+    this.globalConfService.get(null, '/GlobalConfig/AppLicense').subscribe(
+      (res: any) => {
+        if (res && res.data) {
+          this.licAppData = res.data;
+       this.convertToObj();
+          this.sharedService.setApp(this.licAppData);
+        }
+      },
+      (error) => {}
+    );
   }
 
+  convertToObj(){
+    const arrayOfObjects: any = [];
+    for (const key of Object.keys(this.licAppData)) {
+      // arrayOfObjects.push({ key, value: this.licAppData[key] });
+      arrayOfObjects.push({
+        appname: this.licAppData[key].info.name,
+        displayname: this.licAppData[key].info.displayName,
+        license: this.licAppData[key].info.licenseString,
+        numlicense: this.licAppData[key].numLicenses,
+        status: this.licAppData[key].isLicenseValid ?'Valid':'Invalid',
+        appurl: this.licAppData[key].info.url,
+      });
+    }
+    this.dataSource = new MatTableDataSource(arrayOfObjects);
+  }
 }
