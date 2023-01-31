3baf86c02fb612037dd103a4ad6a407247170ce2
Ammad Naeem <ammad.naeem@enterprise64.com>
2023-01-31 12:39:53 +0500

    IM | Functionality Work

--- src/app/induction-manager/super-batch/super-batch.component.ts
+++ src/app/induction-manager/super-batch/super-batch.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatRadioChange } from '@angular/material/radio';
 import { MatTableDataSource } from '@angular/material/table';
 import { ToastrService } from 'ngx-toastr';
 import { RequiredDateStatusComponent } from '../../../app/dialogs/required-date-status/required-date-status.component';
@@ -12,21 +13,13 @@ import { SuperBatchService } from './super-batch.service';
   styleUrls: ['./super-batch.component.scss']
 })
 export class SuperBatchComponent implements OnInit {
-   ELEMENT_DATA: any[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'actions'];
-  dataSource = this.ELEMENT_DATA;
-  user_data:any;
+  displayedColumns: string[] = ['zone', 'totalTransactions', 'weight', 'symbol', 'actions'];
+  dataSource:any;
+  user_data: any;
+  totalTransHeading = 'Single Line Orders';
+  defaultSuperBatchSize:any;
+  isItemNumber:boolean = true;
+  itemNumbers:any;
   constructor(
     private authService: AuthService,
     private dialog: MatDialog,
@@ -36,21 +29,46 @@ export class SuperBatchComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.user_data = this.authService.userData(); 
+    this.user_data = this.authService.userData();
     let payload = {
       "WSID": this.user_data.wsid
     }
-    this.sb_service.get(payload, '/Induction/SuperBatchIndex', 'get').subscribe(res => {
-      console.log(res);
+    this.sb_service.get(payload, '/Induction/SuperBatchIndex').subscribe(res => {
+      const { preferences } = res.data;
+      console.log(res.data);
+      this.itemNumbers = res.data.itemNums;
+      this.defaultSuperBatchSize = preferences.defaultSuperBatchSize;
+      this.getSuperBatchBy('Order');
     })
   }
 
-  openReqDataStatus(){
-    const dialogRef =  this.dialog.open(RequiredDateStatusComponent, {
+  openReqDataStatus() {
+    const dialogRef = this.dialog.open(RequiredDateStatusComponent, {
       height: 'auto',
       width: '100%',
       autoFocus: '__non_existing_element__'
     })
+  }
+
+  getSuperBatchBy(type: any) {
+    let payload = {
+      "Type": type,
+      "ItemNumber": ""
+    }
+    this.sb_service.get(payload, '/Induction/ItemZoneDataSelect').subscribe(res => {
+        console.log(res);
+        this.dataSource = res.data;
+    });
+  }
+  onChangeBatch($event: MatRadioChange){
+    console.log($event.source.name, $event.value);
+    if($event.value === 'Item'){
+      this.dataSource = [];
+      this.isItemNumber = false;
+    }
+    else{
+      this.getSuperBatchBy($event.value);
+    }
   }
 
 }
