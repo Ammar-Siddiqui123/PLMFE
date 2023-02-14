8260225e32963eb488cefa13b624d4b068b79958
Umer Aslam <umer.aslam@enterprise64.com>
2023-02-15 04:24:19 +0500

    PLM-2240 create transaction in progress

--- src/app/admin/dialogs/supplier-item-id/supplier-item-id.component.ts
+++ src/app/admin/dialogs/supplier-item-id/supplier-item-id.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { TransactionService } from '../../transaction/transaction.service';
 
 @Component({
   selector: 'app-supplier-item-id',
   templateUrl: './supplier-item-id.component.html',
-  styleUrls: ['./supplier-item-id.component.scss']
+  styleUrls: ['./supplier-item-id.component.scss'],
 })
 export class SupplierItemIdComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  supplierID;
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private toastr: ToastrService,
+    private transactionService: TransactionService
+  ) {
+    this.supplierID=data.supplierID
   }
 
+  ngOnInit(): void {}
 }
--- src/app/admin/dialogs/temporary-manual-order-number-add/temporary-manual-order-number-add.component.ts
+++ src/app/admin/dialogs/temporary-manual-order-number-add/temporary-manual-order-number-add.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { TransactionService } from '../../transaction/transaction.service';
 
 @Component({
   selector: 'app-temporary-manual-order-number-add',
   templateUrl: './temporary-manual-order-number-add.component.html',
-  styleUrls: ['./temporary-manual-order-number-add.component.scss']
+  styleUrls: ['./temporary-manual-order-number-add.component.scss'],
 })
 export class TemporaryManualOrderNumberAddComponent implements OnInit {
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private toastr: ToastrService,
+    private transactionService: TransactionService
+  ) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
