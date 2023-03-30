d943c6704fc9b6473346c626b3d16d8c5038fc3d
Umer Aslam <umer.aslam@enterprise64.com>
2023-03-30 12:30:01 +0500

    global config removed usernameapp api

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -6,96 +6,98 @@ import { SetColumnSeqService } from '../admin/dialogs/set-column-seq/set-column-
   providedIn: 'root',
 })
 export class SharedService {
-
-  constructor(private columnSequence : SetColumnSeqService) { }
-  loadMenu : boolean = false;
+  constructor(private columnSequence: SetColumnSeqService) {}
+  loadMenu: boolean = false;
   private data: any;
   private menuData = new Subject<any>();
   public SidebarMenupdate = new Subject<any>();
   menuData$ = this.menuData.asObservable();
 
-  private appData:any;
-  startMenu: Subject<any> = new Subject<any>(); 
+  private appData: any;
+  startMenu: Subject<any> = new Subject<any>();
   updateAdminMenuObserver: Subject<boolean> = new Subject<boolean>(); // observing that bool
-  updateInductionAdminObserver: Subject<any> = new Subject<any>(); 
-  orderStatusObserver: Subject<any> = new Subject<any>(); 
-  itemObserver: Subject<any> = new Subject<any>(); 
-  historyItemObserver: Subject<any> = new Subject<any>(); 
+  updateInductionAdminObserver: Subject<any> = new Subject<any>();
+  orderStatusObserver: Subject<any> = new Subject<any>();
+  itemObserver: Subject<any> = new Subject<any>();
+  historyItemObserver: Subject<any> = new Subject<any>();
   reprocessItemObserver: Subject<any> = new Subject<any>();
   orderStatusObjObserver: Subject<any> = new Subject<any>();
   orderStatusSendOrderObserver: Subject<any> = new Subject<any>();
-  historyLocObserver: Subject<any> = new Subject<any>();  
-  appRestrictionObserver: Subject<any> = new Subject<any>();  
-  updateReprocessObserver: Subject<any> = new Subject<any>();  
-  updateToteFilterCheckObserver:Subject<any>=new Subject<any>();
-  updateCompDateObserver:Subject<any>=new Subject<any>();
-  updateOrderStatusSelectObserver:Subject<any>=new Subject<any>();
+  historyLocObserver: Subject<any> = new Subject<any>();
+  appRestrictionObserver: Subject<any> = new Subject<any>();
+  updateReprocessObserver: Subject<any> = new Subject<any>();
+  updateToteFilterCheckObserver: Subject<any> = new Subject<any>();
+  updateCompDateObserver: Subject<any> = new Subject<any>();
+  updateOrderStatusSelectObserver: Subject<any> = new Subject<any>();
 
   resetSidebar() {
     this.startMenu.next(true);
-  }     
+  }
 
-  updateSidebar(){
+  updateSidebar() {
     this.loadMenu = !this.loadMenu;
     return this.loadMenu;
   }
-  
-  updateAdminMenu()
-  {
+
+  updateAdminMenu() {
     this.updateAdminMenuObserver.next(true);
   }
 
-  updateInductionAdminMenu(menu)    // on side menu update induction menu 
-  {
+  updateInductionAdminMenu(
+    menu // on side menu update induction menu
+  ) {
     this.updateInductionAdminObserver.next(menu);
   }
- 
-  updateOrderStatus(order){    // order status observer for selecting order number when passing toteid and set order fields 
+
+  updateOrderStatus(order) {
+    // order status observer for selecting order number when passing toteid and set order fields
     this.orderStatusObserver.next(order);
-
   }
-  updateItemTransaction(item){   // changes select field to item number and passes item number to field in Open Transaction 
+  updateItemTransaction(item) {
+    // changes select field to item number and passes item number to field in Open Transaction
     this.itemObserver.next(item);
-
   }
-  updateTransactionHistory(item){   //changes select field to item number and passes item number to field in Transaction History 
+  updateTransactionHistory(item) {
+    //changes select field to item number and passes item number to field in Transaction History
     this.historyItemObserver.next(item);
-
   }
-  updateTransactionLocHistory(loc){   //changes select field to location and passes location to field in Transaction History 
+  updateTransactionLocHistory(loc) {
+    //changes select field to location and passes location to field in Transaction History
     this.historyLocObserver.next(loc);
-
   }
-  updateTransactionReprocess(item){   //changes select field to item number and passes item number to field in Rreprocess 
+  updateTransactionReprocess(item) {
+    //changes select field to item number and passes item number to field in Rreprocess
     this.reprocessItemObserver.next(item);
-
   }
-  updateFilterByTote(obj){   // passing data of coming from filter of tote passing to order list for filtering
-    this.orderStatusObjObserver.next(obj)
+  updateFilterByTote(obj) {
+    // passing data of coming from filter of tote passing to order list for filtering
+    this.orderStatusObjObserver.next(obj);
   }
-  updateAppVerification(isVerified?){
+  updateAppVerification(isVerified?) {
     this.appRestrictionObserver.next(isVerified);
   }
 
-  updateReprocess(obj?){   // passing item number and order number in reprocess transaction
+  updateReprocess(obj?) {
+    // passing item number and order number in reprocess transaction
     this.updateReprocessObserver.next(obj);
   }
 
-  updateToteFilterCheck(isChecked){  // toggle check box if tote filter present in order status
-    this.updateToteFilterCheckObserver.next(isChecked)
+  updateToteFilterCheck(isChecked) {
+    // toggle check box if tote filter present in order status
+    this.updateToteFilterCheckObserver.next(isChecked);
   }
-  
-  updateOrderStatusOrderNo(orderNumber){ 
+
+  updateOrderStatusOrderNo(orderNumber) {
     this.orderStatusSendOrderObserver.next(orderNumber);
   }
-  updateCompDate(selectType){ 
+  updateCompDate(selectType) {
     this.updateCompDateObserver.next(selectType);
   }
 
-  updateOrderStatusSelect(obj){
-    this.updateOrderStatusSelectObserver.next(obj)
+  updateOrderStatusSelect(obj) {
+    this.updateOrderStatusSelectObserver.next(obj);
   }
-  getSidebarStatus(){
+  getSidebarStatus() {
     return this.loadMenu;
   }
 
@@ -105,7 +107,7 @@ export class SharedService {
   getData() {
     return this.data;
   }
-  
+
   setApp(data: any) {
     this.appData = data;
   }
@@ -113,30 +115,30 @@ export class SharedService {
     return this.appData;
   }
 
-
-  updateLoggedInUser(userName:any,wsid:any,menu:any)
-  {
+  updateLoggedInUser(userName: any, wsid: any, menu: any) {
     let appName;
-    if(menu.includes('/admin')){
+    if (menu.includes('/admin')) {
       appName = 'Admin';
     }
-    if(menu.includes('/InductionManager')){
+    if (menu.includes('/InductionManager')) {
       appName = 'Induction Manager';
     }
-    this.columnSequence.updateAppName(userName,wsid,appName).subscribe((res: any) => {
-      (error) => {
-        // alert(error);
-         }
-         });
+    if (menu.includes('/globalconfig')) return;
+    else {
+      this.columnSequence
+        .updateAppName(userName, wsid, appName)
+        .subscribe((res: any) => {
+          (error) => {
+            // alert(error);
+          };
+        });
+    }
   }
 
   setMenuData(value: any) {
-    
-
-    
     this.menuData.next(value);
   }
-  BroadCastMenuUpdate(str:any){
-    this.SidebarMenupdate.next(str)
+  BroadCastMenuUpdate(str: any) {
+    this.SidebarMenupdate.next(str);
   }
 }
