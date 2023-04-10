0e38920a43a5a7df5a2095d5e199eb763d5fc068
Abdullah-Haroon004 <abdullah.haroon@enterprise64.com>
2023-04-10 14:14:33 +0500

    -- Current Order Tab

--- src/app/admin/system-replenishment/system-replenishment.component.ts
+++ src/app/admin/system-replenishment/system-replenishment.component.ts
@@ -20,6 +20,29 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
 ];
+
+export interface NewPeriodicElement {
+transtype: string;
+ordernumber: string;
+priority: string;
+requireddate: string;
+userfield1: string;
+userfield2: string;
+userfield3: string;
+}
+
+const ELEMENT_NEWDATA: NewPeriodicElement[] = [
+  {transtype: '05', ordernumber: '05', priority: '05', requireddate: '05', userfield1: '05', userfield2: '05', userfield3: '05'},
+  {transtype: '10A', ordernumber: '10A', priority: '10A', requireddate: '10A', userfield1: '10A', userfield2: '10A', userfield3: '10A'},
+  {transtype: '05', ordernumber: '05', priority: '05', requireddate: '05', userfield1: '05', userfield2: '05', userfield3: '05'},
+  {transtype: '10A', ordernumber: '10A', priority: '10A', requireddate: '10A', userfield1: '10A', userfield2: '10A', userfield3: '10A'},
+  {transtype: '05', ordernumber: '05', priority: '05', requireddate: '05', userfield1: '05', userfield2: '05', userfield3: '05'},
+  {transtype: '10A', ordernumber: '10A', priority: '10A', requireddate: '10A', userfield1: '10A', userfield2: '10A', userfield3: '10A'},
+  {transtype: '05', ordernumber: '05', priority: '05', requireddate: '05', userfield1: '05', userfield2: '05', userfield3: '05'},
+  {transtype: '05', ordernumber: '05', priority: '05', requireddate: '05', userfield1: '05', userfield2: '05', userfield3: '05'},
+
+];
+
 @Component({
   selector: 'app-system-replenishment',
   templateUrl: './system-replenishment.component.html',
@@ -27,8 +50,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class SystemReplenishmentComponent implements OnInit {
 
-  displayedColumns: string[] = ['select','position', 'name', 'weight', 'symbol', 'ex', 'srno','replishment','case','transaction','replenish','exists','allocated_pick', 'action'];
+  displayedColumns: string[] = ['select','position', 'name', 'weight', 'symbol', 'ex', 'srno','replishment','case','transaction','replenish','exists','allocated_pick', 'action', ];
   tableData = ELEMENT_DATA;
+
+  displayedColumns2: string[] = ['transtype', 'ordernumber', 'priority', 'requireddate', 'userfield1', 'userfield2', 'userfield3' ];
+  newData = ELEMENT_NEWDATA;
 
 
   constructor(private dialog: MatDialog,) { }
@@ -37,3 +63,4 @@ export class SystemReplenishmentComponent implements OnInit {
   }
 
 }
+
