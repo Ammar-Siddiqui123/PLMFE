2ae5e9fd1527db6df161ecc431bf0d37b69b8ed5
Umer Aslam <umer.aslam@enterprise64.com>
2023-04-12 17:09:37 +0500

    Cycle Count Crete Count

--- src/app/admin/cycle-counts/create-count-batches/create-count-batches.component.ts
+++ src/app/admin/cycle-counts/create-count-batches/create-count-batches.component.ts
@@ -8,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class CreateCountBatchesComponent implements OnInit {
   countQueValue: any = 0;
   constructor() {}
+  selectedIndex: number = 0;
+  updateQueue: any;
 
   ngOnInit(): void {}
   getCount(value) {
     this.countQueValue = value;
+  }
+  public nextStep(e) {
+    this.selectedIndex += 1;
+      this.updateQueue=e
+}
+
+  tabChanged({ index }) {
+    this.selectedIndex = index;
   }
 }
