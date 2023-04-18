31831455602f847b93915585691320baedb9941d
Shan <shan.asghar@enterprise64.com>
2023-04-18 10:09:34 +0500

    service added

---
+++ src/app/consolidation-manager/consolidation-manager.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BaseService } from 'src/app/services/base-service.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ConsolidationManagerService extends BaseService {
+
+  constructor(http: HttpClient) {
+    super(http);
+  }
+}
