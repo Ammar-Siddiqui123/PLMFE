f5368b02448712b743a7f1891eda3572ebf48475
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-04-18 12:50:05 +0500

    update

--- src/app/admin/system-replenishment/sr-current-order/sr-current-order.component.ts
+++ src/app/admin/system-replenishment/sr-current-order/sr-current-order.component.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { SystemReplenishmentService } from '../system-replenishment.service';
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/init/auth.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-sr-current-order',
@@ -11,19 +12,19 @@ import { AuthService } from 'src/app/init/auth.service';
 })
 export class SrCurrentOrderComponent implements OnInit {
 
-  displayedColumns2: string[] = ['itemNumber', 'transactionType', 'warehouse', 'zone', 'carousel', 'row', 'shelf', 'bin', 'rowNumber', 'lotNumber', 'transactionQuantity', 'description', 'orderNumber', 'unitOfMeasure', 'batchPickID', 'serialNumber', 'completedDate', 'printDate'];
+  displayedColumns2: string[] = ['itemNumber', 'transactionType', 'warehouse', 'zone', 'carousel', 'row', 'shelf', 'bin', 'cell', 'lotNumber', 'transactionQuantity', 'description', 'orderNumber', 'unitOfMeasure', 'batchPickID', 'serialNumber', 'completedDate', 'printDate'];
   noOfPicks: number = 0;
   noOfPutAways: number = 0;
   public userData: any;
   tablePayloadObj: any = {
     draw: 0,
-    start: 1,
-    length: 11,
+    start: 0,
+    length: 10,
     searchString: "",
     searchColumn: "",
     sortColumn: "",
     sortDir: "asc",
-    status: "",
+    status: "All",
     filter: "1=1",
     username: "",
     wsid: ""
@@ -31,6 +32,26 @@ export class SrCurrentOrderComponent implements OnInit {
   tableData: any = [];
   filteredTableData: any = [];
   tableDataTotalCount: number = 0;
+  searchColumnOptions: any = [
+    { value: 'Item Number',viewValue:'Item Number', sortColumn:'0'},
+    { value: 'Trans Type',viewValue:'Trans Type', sortColumn:'1'},
+    { value: 'Warehouse',viewValue:'Warehouse', sortColumn:'2'},
+    { value: 'Zone',viewValue:'Zone', sortColumn:'3'},
+    { value: 'Carsl',viewValue:'Carsl', sortColumn:'4'},
+    { value: 'Row',viewValue:'Row', sortColumn:'5'},
+    { value: 'Shelf',viewValue:'Shelf', sortColumn:'6'},
+    { value: 'Bin',viewValue:'Bin', sortColumn:'7'},
+    { value: 'Cell',viewValue:'Cell', sortColumn:'8'},
+    { value: 'Lot Number',viewValue:'Lot Number', sortColumn:'9'},
+    { value: 'Trans Qty',viewValue:'Trans Qty', sortColumn:'10'},
+    { value: 'Description',viewValue:'Description', sortColumn:'11'},
+    { value: 'Order Number',viewValue:'Order Number', sortColumn:'12'},
+    { value: 'UofM',viewValue:'UofM', sortColumn:'13'},
+    { value: 'Batch Pick ID',viewValue:'Batch Pick ID', sortColumn:'14'},
+    { value: 'Serial Number',viewValue:'Serial Number', sortColumn:'15'},
+    { value: 'Comp Date',viewValue:'Comp Date', sortColumn:'16'},
+    { value: 'Print Date',viewValue:'Print Date', sortColumn:'17'},
+  ];
 
   constructor(
     private dialog: MatDialog,
@@ -52,6 +73,7 @@ export class SrCurrentOrderComponent implements OnInit {
         this.tableData = res.data.sysTable;
         this.tableDataTotalCount = res.data.recordsTotal;
         this.filteredTableData = JSON.parse(JSON.stringify(this.tableData));
+        this.updateCounts();
       } else {
         this.toastr.error(res.responseMessage, 'Error!', {
           positionClass: 'toast-bottom-right',
@@ -61,4 +83,111 @@ export class SrCurrentOrderComponent implements OnInit {
     });
   }
 
+  updateCounts(){
+    this.noOfPutAways = this.filteredTableData.filter((item: any) => item.transactionType == 'Put Away').length;
+    this.noOfPicks = this.filteredTableData.filter((item: any) => item.transactionType == 'PICK').length;
+  }
+
+  paginatorChange(event: PageEvent) {
+    if(event.previousPageIndex != undefined && event.pageIndex > event.previousPageIndex) {
+      this.tablePayloadObj.start = this.tablePayloadObj.start + event.pageSize;
+    }
+    else {
+      this.tablePayloadObj.start = this.tablePayloadObj.start - event.pageSize;
+    }
+    this.tablePayloadObj.length = event.pageSize;
+    this.newReplenishmentOrders();
+  }
+
+  actionChange(event: any) {
+    if (event == 'Print Orders') {
+      this.printOrders();
+    }
+    else if (event == 'Print Labels') {
+      this.printLabels();
+    }
+    else if (event == 'Delete All Orders') {
+      this.deleteAllOrders();
+    }
+    else if (event == 'Delete Shown Orders') {
+      this.deleteShownOrders();
+    }
+    else if (event == 'Delete Range') {
+      this.deleteRange();
+    }
+    else if (event == 'Delete Selected Order') {
+      this.deleteSelectedOrder();
+    }
+    else if (event == 'View All Orders') {
+      this.viewAllOrders();
+    }
+    else if(event == 'View Unprinted Orders'){
+      this.viewUnprintedOrders();
+    }
+  }
+
+  printOrders(){
+    alert("The print service is currently offline");
+  }
+
+  printLabels(){
+    alert("Print Labels Popup Missing");
+  }
+
+  deleteAllOrders(){
+    if (confirm("Are you sure you want to delete all records")) {
+
+    }
+  }
+
+  deleteShownOrders(){
+    if(confirm("Are you sure you want to delete all records that are currently dipslayed")){
+
+    }
+  }
+
+  deleteRange(){
+    alert("Delete Range Popup Missing");
+  }
+
+  deleteSelectedOrder(){
+    alert("Delete Selected Order Confirmation Popup Missing");
+  }
+
+  viewAllOrders(){
+    this.filteredTableData = JSON.parse(JSON.stringify(this.tableData));
+    this.updateCounts();
+  }
+
+  viewUnprintedOrders(){
+    this.tableData = JSON.parse(JSON.stringify(this. filteredTableData));
+    this.filteredTableData = this.filteredTableData.filter((item: any) => item.printDate == '');
+    this.updateCounts();
+  }
+
+  showChange(event: any) {
+    if (event == 'All') {
+      this.tablePayloadObj.status = event;
+      this.newReplenishmentOrders();
+    }
+    else if (event == 'Open') {
+      this.tablePayloadObj.status = event;
+      this.newReplenishmentOrders();
+    }
+    else if (event == 'Completed') {
+      this.tablePayloadObj.status = event;
+      this.newReplenishmentOrders();
+    }
+  }
+
+  searchChange(event: any) {
+    this.tablePayloadObj.searchColumn = event;
+    // this.tablePayloadObj.sortColumn = this.searchColumnOptions.filter((item: any) => item.value == event)[0].sortColumn;
+  }
+
+  search(){
+    if(this.tablePayloadObj.searchColumn != "" && this.tablePayloadObj.searchString != ""){
+      this.newReplenishmentOrders();
+    }
+  }
 }
