85eb8d0b79d08e1848dd522e64f8e39e10d01ff7
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-04-20 14:11:48 +0500

    confirm and packing modals new changes

--- src/app/dialogs/cm-confirm-and-packing/cm-confirm-and-packing.component.ts
+++ src/app/dialogs/cm-confirm-and-packing/cm-confirm-and-packing.component.ts
@@ -4,6 +4,7 @@ import { ConsolidationManagerService } from 'src/app/consolidation-manager/conso
 import { AuthService } from 'src/app/init/auth.service';
 import { MatDialog } from '@angular/material/dialog';
 import { CmConfirmAndPackingProcessTransactionComponent } from '../cm-confirm-and-packing-process-transaction/cm-confirm-and-packing-process-transaction.component';
+import { CmConfirmAndPackingSelectTransactionComponent } from '../cm-confirm-and-packing-select-transaction/cm-confirm-and-packing-select-transaction.component';
 
 @Component({
   selector: 'app-cm-confirm-and-packing',
@@ -11,7 +12,7 @@ import { CmConfirmAndPackingProcessTransactionComponent } from '../cm-confirm-an
   styleUrls: ['./cm-confirm-and-packing.component.scss']
 })
 export class CmConfirmAndPackingComponent implements OnInit {
-  orderNumber:any = "2909782A";
+  orderNumber:any = "123456";
   toteTable:any[]=[];
   ItemNumber:any;
   transTable:any[]=[];
@@ -41,29 +42,16 @@ displayedColumns_1: string[] = ['sT_ID','itemNumber', 'lineNumber',   'transacti
     this.toast.error("An error has occurred",'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000});
 }  
 }
-openScanItem() {
-  let dialogRef = this.dialog.open(CmConfirmAndPackingProcessTransactionComponent, {
-    height: 'auto',
-    width: '96vw',
-    autoFocus: '__non_existing_element__',
-   
-  })
-  dialogRef.afterClosed().subscribe(result => {
-    
-    
-  })
- }
+
 async UnPack(id:any){  
-  this.http.get({id:id},'/Consolidation/ConfirmAndPackingIndex').subscribe((res:any) => {
+  this.http.get({id:id},'/Consolidation/ShipTransUnPackUpdate').subscribe((res:any) => {
     if (res.data == "Fail") {
       this.toast.error("An error has occurred", 'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000});  
-  } else {
-      // $('#ShipTransTable tbody tr.active').find(':nth-child(5)').html('');
-      // $('#ShipTransTable tbody tr.active').find(':nth-child(7)').html('False');
-      // this.transTable[0].invalidate();
+  } else {  
      var index =  this.transTable.findIndex(x=>x.sT_ID == id);
      this.transTable[index].containerID = '';
      this.transTable[index].complete = false;
+      // this.transTable[0].invalidate();
   };
   });
  
@@ -77,6 +65,7 @@ ConfirmAndPackingIndex(){
     wsid: this.userData.wsid, 
   };
  this.http.get(obj,'/Consolidation/ConfirmAndPackingIndex').subscribe((res:any) => {
+  debugger
   this.toteTable = res.data.confPackToteTable;
   this.orderNumber = res.data.orderNumber;
   this.transTable = res.data.confPackShipTransTable;
@@ -100,33 +89,55 @@ async ClickConfirmAll(){
    this.http.get(obj,'/Consolidation/ConfPackScanItemNum').subscribe((res:any) => {
     if (res.data == "Fail") {
       this.toast.error('An error has occurred', 'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000}); 
-  } else {
-      //check preferences for auto printing
-      if (this.PrintPrefs.PrintContLabel == "True") {
-          // getLLPreviewOrPrint('/CM/ConfirmAndPacking/PrintConfPackLabel', {
-          //     OrderNum: $('#ConfPackOrderNum').val(),
-          //     contID: $('#ContID').val()
-          // }, true,'label', 'Container ID Label')
-      };
-
-      if (this.PrintPrefs.PrintContPL == "True") {
-          // getLLPreviewOrPrint('/CM/ConfirmAndPacking/PrintConfPackPrintCont', {
-          //     OrderNum: $('#ConfPackOrderNum').val(),
-          //     contID: $('#ContID').val()
-          // }, true,'report', 'Packing List - Container')
-      };
-
-      if (this.PrintPrefs.PrintOrderPL == "True") {
-          // getLLPreviewOrPrint('/CM/ConfirmAndPacking/PrintConfPackPackList', {
-          //     OrderNum: $('#ConfPackOrderNum').val()
-          // }, true,'report', 'Packing List - Container Order');
-      };
+  } else { 
       location.reload();
     }
   });
   }
 } 
+openScanItem(ItemNumber:any,id: any) {
+  var index= this.transTable.findIndex(x=>x.sT_ID == id);
+  this.transTable[index].active = true;
+  let dialogRef = this.dialog.open(CmConfirmAndPackingProcessTransactionComponent, {
+    height: 'auto',
+    width: '96vw',
+    autoFocus: '__non_existing_element__',
+    data: {ItemNumber:ItemNumber,orderNumber:this.orderNumber,contID:this.contID,confPackTransTable:this.transTable,id:id}
+  })
+  dialogRef.afterClosed().subscribe(result => {
+    if(result == 'ConfirmedPacked'){
+      this.ConfirmedPacked();
+    }  
+  })
+ }
+ openSelectTransaction(ItemNumber:any,id: any) {
+  var index= this.transTable.findIndex(x=>x.sT_ID == id);
+  this.transTable[index].active = true;
+  let dialogRef = this.dialog.open(CmConfirmAndPackingSelectTransactionComponent, {
+    height: 'auto',
+    width: '96vw',
+    autoFocus: '__non_existing_element__',
+    data: {ItemNumber:ItemNumber,orderNumber:this.orderNumber,contID:this.contID,confPackTransTable:this.transTable,id:id}
+  })
+  dialogRef.afterClosed().subscribe(result => {
+    if(result == 'true'){
+      this.transTable[index].containerID = this.contID;
+      this.transTable[index].complete = true; 
+    }  
+  })
+ }
+ ItemKeyUp(){
+  setTimeout(() => {
+    if(this.OldtransTable.length > 0){
+      this.transTable = this.OldtransTable.filter(x=>  x.itemNumber.indexOf(this.ItemNumber) > -1);  
+    }else{
+      this.OldtransTable = this.transTable;
+      this.transTable = this.transTable.filter(x=>  x.itemNumber.indexOf(this.ItemNumber) > -1);  
+    }
+  }, 10);
+ }
 async ScanItemNum($event:any){ 
+  debugger   
   if($event.key == "Enter"){
   var index;
 var searchCount = 0;
@@ -141,9 +152,8 @@ for (var x = 0; x < this.transTable.length; x++) {
         index = x;
     };
 };
-// this.OldtransTable = this.transTable;
-// this.transTable = this.transTable.filter(x=>  x.itemNumber.indexOf($event.target.value) > -1);
-if(searchCount == 0){
+ 
+if(searchCount == 0){ 
   this.toast.error("The desired item number was not found or is already confirmed and packed",'Item Number Issue', { positionClass: 'toast-bottom-right',timeOut: 2000}); 
 } else if (searchCount == 1) {
   var obj : any = {
@@ -155,22 +165,15 @@ if(searchCount == 0){
     wsid: this.userData.wsid
   };
  this.http.get(obj,'/Consolidation/ConfPackProcModalUpdate').subscribe((res:any) => {
-  debugger
+  
   if (res.data == "Fail") {
     this.toast.error('An error has occurred', 'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000});  
 } else if (res.data == "Modal") {
     //show modal here
-    this.http.get({id:obj.id},'/Consolidation/ConfPackProcModal').subscribe((response:any) => {
-      // confPackTransTable.row(index).nodes().to$().click();
-      // $('#ConfPackProcessModal').modal('show');
-      var index =  this.transTable.findIndex(x=>x.itemNumber == $event.target.value);
-      // this.transTable[index].containerID = this.contID;
-      // this.transTable[index].complete = true;
-      // this.transTable = response.data; 
-      // childs components
-      // show modal 
+  this.openSelectTransaction($event.target.value,id);
 
-    });
+    // this.openScanItem($event.target.value,id);
+   
  
 } else {
   var index =  this.transTable.findIndex(x=>x.itemNumber == $event.target.value);
@@ -183,23 +186,7 @@ if(searchCount == 0){
 };
  });
   }else {
-    //pop select modal
-    // within modal execute quant modal functionality
-    this.http.get({orderNumber:this.orderNumber,itemNumber:this.ItemNumber},'/Consolidation/ConfPackSelectDT').subscribe((response:any) => {
-      var index =  this.transTable.findIndex(x=>x.itemNumber == $event.target.value);
-      // this.transTable[index].containerID = this.contID;
-      // this.transTable[index].complete = true;
-      // this.transTable = response.data; 
-      // childs components
-    });
-   
-        // $('#ConfPackSelectModal').modal('show');
- 
-        // $('#ConfPackSelectOrderNum').val($('#ConfPackOrderNum').val());
-        // $('#ConfPackSelectContID').val($('#ContID').val());
-        // confPackSelectTable.clear().draw();
-        // confPackSelectTable.rows.add(dataset).draw(); 
-        // show modal
+   this.openSelectTransaction($event.target.value,id);
 };
 }
 }
