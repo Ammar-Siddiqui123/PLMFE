06203bb9d7c213606054b0b4512009d6e01d8874
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-04-26 17:30:56 +0500

    new changes stagging page

--- src/app/dialogs/cm-confirm-and-packing/cm-confirm-and-packing.component.ts
+++ src/app/dialogs/cm-confirm-and-packing/cm-confirm-and-packing.component.ts
@@ -96,12 +96,12 @@ async ClickConfirmAll(){
 } 
 openScanItem(ItemNumber:any,id: any) {
   var index= this.transTable.findIndex(x=>x.sT_ID == id);
-  this.transTable[index].active = true;
+  this.transTable[index].active = true; 
   let dialogRef = this.dialog.open(CmConfirmAndPackingProcessTransactionComponent, {
     height: 'auto',
     width: '96vw',
     autoFocus: '__non_existing_element__',
-    data: {ItemNumber:ItemNumber,orderNumber:this.orderNumber,contID:this.contID,confPackTransTable:this.transTable,id:id}
+    data: {ItemNumber:ItemNumber,orderNumber:this.orderNumber,contID:this.contID,confPackTransTable:this.transTable,id:id,reasons:this.reasons}
   })
   dialogRef.afterClosed().subscribe(result => {
     if(result == 'ConfirmedPacked'){
@@ -163,16 +163,13 @@ if(searchCount == 0){
     wsid: this.userData.wsid
   };
  this.http.get(obj,'/Consolidation/ConfPackProcModalUpdate').subscribe((res:any) => {
-  
+   
   if (res.data == "Fail") {
     this.toast.error('An error has occurred', 'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000});  
-} else if (res.data == "Modal") {
+} else if (res.data == "Modal" || true) {
     //show modal here
-  this.openScanItem($event.target.value,id);
-   
- 
-} else {
-  var index =  this.transTable.findIndex(x=>x.itemNumber == $event.target.value);
+  this.openScanItem($event.target.value,id);  
+} else {  var index =  this.transTable.findIndex(x=>x.itemNumber == $event.target.value);
   // this.transTable[index].containerID = this.contID;
   // this.transTable[index].complete = true;
   // this.transTable[index].sT_ID.invalidate(); 
--- src/app/dialogs/cm-shipping/cm-shipping.component.ts
+++ src/app/dialogs/cm-shipping/cm-shipping.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { ConsolidationManagerService } from 'src/app/consolidation-manager/consolidation-manager.service';
+import { AuthService } from 'src/app/init/auth.service';
 
 export interface PeriodicElement {
   name: string;
@@ -21,13 +25,109 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class CmShippingComponent implements OnInit {
   
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'ex', 'srno', 'replishment', 'case', 'transaction', 'replenish', 'exists', 'action'];
+  displayedColumns: string[] = ['containerID', 'carrier', 'trackingNum','action'];
   tableData = ELEMENT_DATA;
-
-  constructor() { }
-  
-
-  ngOnInit(): void {
+  userData:any = {};
+  orderNumber:any = '2910098';
+  shippingData:any[]=[];
+  carriers:any[]=[];
+  shippingComp:any = false;
+  shippingPreferences:any={};
+   constructor(private http:ConsolidationManagerService,private authService: AuthService,private toast:ToastrService,private dialog: MatDialog,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<CmShippingComponent>,) { 
+      // this.orderNumber = this.data.orderNumber;
+    this.userData = this.authService.userData();
   }
 
+  ngOnInit(): void {
+    this.ShippingIndex();
+  }
+async ShippingIndex(){
+ var obj : any= {
+    orderNumber: this.orderNumber ,
+    userName: this.userData.userName,
+    wsid: this.userData.wsid
+  }
+ this.http.get(obj,'/Consolidation/ShippingIndex').subscribe((res:any) => {
+  if (res && res.isExecuted) {
+  this.shippingData = res.data.shippingData;
+  this.carriers = res.data.carriers;
+  this.shippingPreferences = res.data.shippingPreferences;
+  this.shippingComp = res.data.shippingComp;
+  this.orderNumber = res.data.orderNumber;
+  }
+});
+}
+async DeleteItem(element:any){
+ var obj :any=  {
+    id: element.id,
+    orderNumber: this.orderNumber,
+    contId: element.containerID,
+    carrier: element.carrier,
+    trackingNum: element.trackingNum,
+    userName: this.userData.username,
+    wsid: this.userData.wsid
+  }
+  this.http.get(obj,'/Consolidation/ShipmentItemDelete').subscribe((res:any) => {
+    if (res && res.isExecuted) {
+      this.ShippingIndex();
+    }
+  });
+} 
+async updateShipmentItem(element:any){
+  var obj :any=  {
+    id: element.id, 
+    carrier: element.carrier,
+    trackingNum: element.trackingNum, 
+    "freight": element.freight,
+    "freight1": element.freight1,
+    "freight2": element.freight2,
+    "weight": element.weight,
+    "length": element.length,
+    "width": element.width,
+    "height": element.height,
+    "cube": element.cube
+  }
+   this.http.get(obj,'/Consolidation/ShipmentItemUpdate').subscribe((res:any) => {
+     if (res && res.isExecuted) {
+       this.ShippingIndex();
+     }
+   });
+ } 
+ async ShippingCompShip(){
+  var obj :any= {
+    orderNumber: this.orderNumber
+  }
+   this.http.get(obj,'/Consolidation/SelCountOfOpenTransactionsTemp').subscribe((res:any) => {
+     if (res) {
+      if (res.data == -1) {
+        this.toast.error("An error has occurred","Error",  { positionClass: 'toast-bottom-right',timeOut: 2000});
+    } else if (res.data == 0) {
+        //call function to complete shipment
+        this.completeShipment();
+    } else {
+        //for temp
+        var otherconf = confirm("Back Orders exist for this order number. Still complete shipment?");
+        if (otherconf) {
+          this.completeShipment();
+        };
+    };
+     }
+   });
+ }  
+ async completeShipment(){
+  var obj : any= {
+    orderNumber: this.orderNumber ,
+    userName: this.userData.userName,
+    wsid: this.userData.wsid
+  }
+  this.http.get(obj,'/Consolidation/CompleteShipment').subscribe((res:any) => {
+    if (res && res.isExecuted) {
+       this.dialogRef.close(true);
+    }else{
+      this.toast.error("An error has occurred","Error",  { positionClass: 'toast-bottom-right',timeOut: 2000});
+    }
+  });
+ }
 }
