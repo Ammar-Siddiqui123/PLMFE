351e67f4d5c64ee2682dc882075daa5f88cf1e6f
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-04-28 15:42:52 +0500

    new fixing

--- src/app/dialogs/cm-confirm-and-packing/cm-confirm-and-packing.component.ts
+++ src/app/dialogs/cm-confirm-and-packing/cm-confirm-and-packing.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from 'src/app/init/auth.service';
 import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { CmConfirmAndPackingProcessTransactionComponent } from '../cm-confirm-and-packing-process-transaction/cm-confirm-and-packing-process-transaction.component';
 import { CmConfirmAndPackingSelectTransactionComponent } from '../cm-confirm-and-packing-select-transaction/cm-confirm-and-packing-select-transaction.component';
+import { contains } from 'jquery';
 
 @Component({
   selector: 'app-cm-confirm-and-packing',
@@ -49,8 +50,18 @@ displayedColumns_1: string[] = ['sT_ID','itemNumber', 'lineNumber',   'transacti
    
   }
   async NextContID(){ 
-    this.ConfirmAndPackingIndex(true)
+    var obj : any = {
+      orderNumber: this.orderNumber,
+      username: this.userData.userName,
+      wsid: this.userData.wsid, 
+    };
+   this.http.get(obj,'/Consolidation/SelContIDConfirmPack').subscribe((res:any) => { 
+    if(res.data == ''){
+      this.toast.error("An error has occurred",'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000});
+    }else this.contID = res.data;
+   });
 }
+
 
 async UnPack(id:any){  
   this.http.get({id:id},'/Consolidation/ShipTransUnPackUpdate').subscribe((res:any) => {
@@ -67,7 +78,7 @@ async UnPack(id:any){
 }
  
  
-ConfirmAndPackingIndex(NextContID:any = null){ 
+ConfirmAndPackingIndex(){ 
 
 
 if(this.orderNumber != ""){
@@ -83,9 +94,7 @@ if(this.orderNumber != ""){
   this.contIDDrop = res.data.confPackContIDDrop;
   this.confPackEnable = res.data.confPackEnable;
   this.contID = res.data.contIDConfirmPack;
-  if (this.contID == '' && NextContID) {
-    this.toast.error("An error has occurred",'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000});
-}  
+ 
   this.reasons = res.data.adjustmentReason;
   this.shipComp = res.data.confPackShipComp;
   this.PrintPrefs = res.data.confPackPrintPrefs; 
@@ -182,7 +191,7 @@ if(searchCount == 0){
    
   if (res.data == "Fail") {
     this.toast.error('An error has occurred', 'Error!', { positionClass: 'toast-bottom-right',timeOut: 2000});  
-} else if (res.data == "Modal" || true) {
+} else if (res.data == "Modal") {
     //show modal here
   this.openScanItem($event.target.value,id);  
 } else {  var index =  this.transTable.findIndex(x=>x.itemNumber == $event.target.value);
