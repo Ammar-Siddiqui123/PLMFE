dc438651541658288951d878ec0236dfc20f637e
Shan <shan.asghar@enterprise64.com>
2023-05-05 23:35:20 +0500

    Update environment.stage.ts

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: "https://staging-e64.com:9012/api",
+    apiUrl: "https://staging-e64.com:9011/api",
     userName: "pickpro",
     password: "Admin$11"
   };
