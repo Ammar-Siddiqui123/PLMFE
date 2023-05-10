10bddb433857114d39fce7b05a2c446f8028669e
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-05-10 19:28:24 +0500

    update

--- src/app/dialogs/om-create-orders/om-create-orders.component.ts
+++ src/app/dialogs/om-create-orders/om-create-orders.component.ts
@@ -113,6 +113,7 @@ export class OmCreateOrdersComponent implements OnInit {
     dialogRef.afterClosed().subscribe(result => {
       debugger;
       if (result) {
+        this.createOrdersDTPayload.orderNumber = result.orderNumber;
         this.createOrdersDT();
       }
     });
@@ -319,6 +320,7 @@ export class OmCreateOrdersComponent implements OnInit {
   }
 
   onContextMenuCommand(SelectedItem: any, FilterColumnName: any, Condition: any, Type: any) {
+    debugger;
     this.FilterString = this.filterService.onContextMenuCommand(SelectedItem, FilterColumnName, "clear", Type);
     this.FilterString = this.filterService.onContextMenuCommand(SelectedItem, FilterColumnName, Condition, Type);
     this.createOrdersDTPayload.filter = this.FilterString != "" ? this.FilterString : "1=1";
