7c8efa3533e2f70634f2c409292f4d651704ff1f
Umer Aslam <umer.aslam@enterprise64.com>
2023-05-12 17:06:19 +0500

    response changed from an API

--- src/app/induction-manager/pallet-receiving/pallet-receiving.component.ts
+++ src/app/induction-manager/pallet-receiving/pallet-receiving.component.ts
@@ -56,7 +56,7 @@ export class PalletReceivingComponent implements OnInit {
       this.imService
         .get(payloadTote, '/Induction/ValidateTotesForPutAways') //validate tote
         .subscribe((response: any) => {
-          if (response.data != '') {
+          if (response.data) {
             let payloadItem = {
               item: this.processForm.value.itemNo,
               username: this.userData.userName,
@@ -65,7 +65,7 @@ export class PalletReceivingComponent implements OnInit {
             this.imService
               .get(payloadItem, '/Induction/ValidateItem') //validate item number
               .subscribe((response: any) => {
-                if (response.data != '') {
+                if (response.data) {
                   // if item number is valid process pallet
                   let payload = {
                     toteId: this.processForm.value.toteID,
