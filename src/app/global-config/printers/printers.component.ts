54459145e1e377316ed77d9b48e0e6bd764a5ce8
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-05-22 12:53:59 +0500

    CRUD Operations

--- src/app/global-config/printers/printers.component.ts
+++ src/app/global-config/printers/printers.component.ts
@@ -4,6 +4,7 @@ import { DeleteConfirmationComponent } from 'src/app/admin/dialogs/delete-confir
 import { GlobalconfigService } from '../globalconfig.service';
 import { AuthService } from 'src/app/init/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import labels from '../../labels/labels.json'
 
 @Component({
   selector: 'app-printers',
@@ -13,13 +14,14 @@ import { ToastrService } from 'ngx-toastr';
 export class PrintersComponent implements OnInit {
   sideBarOpen: boolean = true;
   displayedColumns: string[] = ['printerName', 'printerAddress', 'labelPrinter', 'actions'];
-  toteTable: any[] = ['10', '10', '10', '10', '10', '10'];
   running: boolean = false;
   userData: any;
+  allPinters: any[] = [];
+  addingNew = false;
 
   constructor(
     private dialog: MatDialog,
-    private globalconfigService : GlobalconfigService,
+    private globalconfigService: GlobalconfigService,
     private authService: AuthService,
     private toastr: ToastrService,
   ) { }
@@ -27,20 +29,39 @@ export class PrintersComponent implements OnInit {
   ngOnInit(): void {
     this.userData = this.authService.userData();
     this.getServiceStatus();
+    this.GetAllPrinters();
   }
 
-  getServiceStatus(loader: boolean = false){
+  GetAllPrinters() {
+    let payload = {
+      "userName": this.userData.userName,
+      "wsid": this.userData.wsid,
+      "appName": ""
+    };
+    this.globalconfigService.get(payload, '/GlobalConfig/GetAllPrinters').subscribe((res: any) => {
+      if (res.isExecuted && res.data) {
+        this.allPinters = res.data;
+        this.allPinters.forEach((element: any) => {
+          element.labelPrinter = element.label == "Able to Print Labels" ? "Yes" : "No";
+          element.isNew = false;
+          element.currentPrinter = element.printer;
+        });
+      }
+    });
+  }
+
+  getServiceStatus(loader: boolean = false) {
     let payload: any = {};
-    this.globalconfigService.get(payload, '/GlobalConfig/StatusPrintService',loader).subscribe((res: any) => {
+    this.globalconfigService.get(payload, '/GlobalConfig/StatusPrintService', loader).subscribe((res: any) => {
       if (res.isExecuted && res.data) {
         this.running = res.data;
       }
     });
   }
 
-  startService(loader: boolean = false){
+  startService(loader: boolean = false) {
     let payload: any = {};
-    this.globalconfigService.get(payload, '/GlobalConfig/StartPrintService',loader).subscribe((res: any) => {
+    this.globalconfigService.get(payload, '/GlobalConfig/StartPrintService', loader).subscribe((res: any) => {
       if (res.isExecuted && res.data) {
         this.running = true;
         this.toastr.success("Service start was successful.", 'Success!', {
@@ -48,7 +69,7 @@ export class PrintersComponent implements OnInit {
           timeOut: 2000
         });
       }
-      else{
+      else {
         this.toastr.error("Service start was unsuccessful. Please try again or contact Scott Tech for support.", 'Error!', {
           positionClass: 'toast-bottom-right',
           timeOut: 2000
@@ -57,9 +78,9 @@ export class PrintersComponent implements OnInit {
     });
   }
 
-  stopService(loader: boolean = false){
+  stopService(loader: boolean = false) {
     let payload: any = {};
-    this.globalconfigService.get(payload, '/GlobalConfig/StopPrintService',loader).subscribe((res: any) => {
+    this.globalconfigService.get(payload, '/GlobalConfig/StopPrintService', loader).subscribe((res: any) => {
       if (res.isExecuted && res.data) {
         this.running = false;
         this.toastr.success("Service stop was successful.", 'Success!', {
@@ -67,7 +88,7 @@ export class PrintersComponent implements OnInit {
           timeOut: 2000
         });
       }
-      else{
+      else {
         this.toastr.error("Service stop encountered an error. Please try again or contact Scott Tech for support.", 'Error!', {
           positionClass: 'toast-bottom-right',
           timeOut: 2000
@@ -76,9 +97,9 @@ export class PrintersComponent implements OnInit {
     });
   }
 
-  restartService(loader: boolean = false){
+  restartService(loader: boolean = false) {
     let payload: any = {};
-    this.globalconfigService.get(payload, '/GlobalConfig/RestartPrintService',loader).subscribe((res: any) => {
+    this.globalconfigService.get(payload, '/GlobalConfig/RestartPrintService', loader).subscribe((res: any) => {
       if (res.isExecuted && res.data) {
         this.running = true;
         this.toastr.success("Service restart was successful.", 'Success!', {
@@ -86,7 +107,7 @@ export class PrintersComponent implements OnInit {
           timeOut: 2000
         });
       }
-      else{
+      else {
         this.toastr.error("Service restart was unsuccessful. Please try again or contact Scott Tech for support.", 'Error!', {
           positionClass: 'toast-bottom-right',
           timeOut: 2000
@@ -106,67 +127,89 @@ export class PrintersComponent implements OnInit {
       autoFocus: '__non_existing_element__',
       data: {
         mode: 'remove-printer',
-        ErrorMessage: `Are you sure you wish to delete this printer: ${printer.name}?`,
+        ErrorMessage: `Are you sure you wish to delete this printer: ${printer.isNew ? 'New' : printer.printer}?`,
         action: 'delete'
       },
     });
     dialogRef.afterClosed().subscribe((result) => {
       if (result === 'Yes') {
-        if (printer == "New") {
-          // $this.parent().parent().remove();
-          // $('#printer_add').removeAttr('disabled');
+        if (printer.isNew) {
+          this.allPinters = this.allPinters.filter((item: any) => item.printer != printer.printer);
+          this.addingNew = false;
         }
         else {
-          // let payload = {
-          //   "ids": ids,
-          //   "user": this.userData.userName,
-          //   "wsid": this.userData.wsid
-          // };
-          // this.orderManagerService.get(payload, '/globalconfig/deletePrinter').subscribe((res: any) => {
-          //   if (res.isExecuted && res.data) {
-          //     this.toastr.success(labels.alert.delete, 'Success!', {
-          //       positionClass: 'toast-bottom-right',
-          //       timeOut: 2000
-          //     });
-          //     $this.parent().parent().remove();
-          //   } else {
-          //     this.toastr.error("Delete Failed", 'Error!', {
-          //       positionClass: 'toast-bottom-right',
-          //       timeOut: 2000
-          //     });
-          //   }
-          // });
+          let payload = {
+            "printerName": printer.printer
+          };
+          this.globalconfigService.get(payload, '/globalconfig/deletePrinter').subscribe((res: any) => {
+            if (res.isExecuted && res.data) {
+              this.toastr.success(labels.alert.delete, 'Success!', {
+                positionClass: 'toast-bottom-right',
+                timeOut: 2000
+              });
+              this.allPinters = this.allPinters.filter((item: any) => item.printer != printer.printer);
+            } else {
+              this.toastr.error("Delete Failed", 'Error!', {
+                positionClass: 'toast-bottom-right',
+                timeOut: 2000
+              });
+            }
+          });
         }
       }
     });
   }
 
-  SavePrinter(printer: any) {
-    // if (printer == "New") {
-    //   config.server.insertNewPrinter($('[name="New_printerinput"]').val(), $('[name="New_addinput"]').val(), $this.parent().siblings().children('.toggles').data('toggles').active).done(function (data) {
-    //     if (data == '') {
-    //       $('#printer_add').removeAttr('disabled');
-    //       $this.attr('disabled', 'disabled');
-    //       var newName = $('[name="New_printerinput"]');
-    //       newName.attr('name', newName.val() + '_name')
-    //       $('[name="New_addinput"]').attr('name', newName.val() + '_add');
-    //       $this.parent().siblings().children('.toggles').data('name', newName.val());
-    //       $this.parent().siblings().children('.remove-printer').attr('name', newName.val());
-    //       $this.attr('name', newName.val());
+  addNewPrinter() {
+    this.addingNew = true;
+    this.allPinters.push({ printer: '', printerAdd: '', label: 'Not Able to Print Labels', labelPrinter: 'No', isNew: true });
+    this.allPinters = [...this.allPinters];
+  }
 
-    //     } else {
-    //       alert(data);
-    //     };
-    //   });
-    // } else {
-    //   config.server.updateCurrentPrinter(printer, $('[name="' + printer + '_name"]').val(), $('[name="' + printer + '_add"]').val(), $this.parent().siblings().children('.toggles').data('toggles').active).done(function (data) {
-    //     if (data != '') {
-    //       alert(data);
-    //     } else {
-    //       $this.attr('disabled', 'disabled');
-    //     };
-    //   });
-    // };
+  SavePrinter(printer: any) {
+    if (printer.isNew) {
+      let payload = {
+        "printerName": printer.printer,
+        "printerString": printer.printerAdd,
+        "label": printer.labelPrinter == 'Yes' ? true : false
+      };
+      this.globalconfigService.get(payload, '/globalconfig/InsertNewPrinter').subscribe((res: any) => {
+        if (res.isExecuted) {
+          this.toastr.success(labels.alert.success, 'Success!', {
+            positionClass: 'toast-bottom-right',
+            timeOut: 2000
+          });
+          printer.isNew = false;
+          this.addingNew = false;
+        } else {
+          this.toastr.error(labels.alert.went_worng, 'Error!', {
+            positionClass: 'toast-bottom-right',
+            timeOut: 2000
+          });
+        }
+      });
+    }
+    else {
+      let payload = {
+        "currentPrinter": printer.currentPrinter,
+        "newPrinter": printer.printer,
+        "printerString": printer.printerAdd,
+        "label": printer.labelPrinter == 'Yes' ? true : false
+      };
+      this.globalconfigService.get(payload, '/globalconfig/UpdateCurrentPrinter').subscribe((res: any) => {
+        if (res.isExecuted) {
+          this.toastr.success(labels.alert.update, 'Success!', {
+            positionClass: 'toast-bottom-right',
+            timeOut: 2000
+          });
+        } else {
+          this.toastr.error(labels.alert.went_worng, 'Error!', {
+            positionClass: 'toast-bottom-right',
+            timeOut: 2000
+          });
+        }
+      });
+    }
   }
 }
 
