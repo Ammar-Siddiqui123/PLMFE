3814ed068193468ae8ec422e2f9f4e23e715c9e1
Shan <shan.asghar@enterprise64.com>
2023-06-02 10:39:25 +0500

    For testing Pipeline

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiUrl: "https://staging-e64.com:9010/api",
   // apiUrl: "http://localhost:63590/api",  
   userName: "pickpro",
-  ///////////////////////////////////////////
+  ////////////////////////////////////////////
   password: "Admin$11"
 };
 
