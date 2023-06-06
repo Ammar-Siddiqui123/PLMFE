b31f92187a9a8f90cf0c66639b520ce4214220ec
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-06-06 18:12:32 +0500

    PLM-4067,PLM-4068,PLM-4140 fixes

--- src/app/admin/location-assignment/location-assignment.component.ts
+++ src/app/admin/location-assignment/location-assignment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationAssignmentService } from './location-assignment.service';
+import { AuthService } from 'src/app/init/auth.service';
 
 @Component({
   selector: 'app-location-assignment',
@@ -7,14 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocationAssignmentComponent implements OnInit {
 
-  constructor() {
-    
-   }
+  countLabel:string = "Count";
+  pickLabel:string = "Pick";
+  putAwayLabel:string = "Put Away";
+  public userData: any;
+
+  constructor(
+    private locationService: LocationAssignmentService,
+    private authservice : AuthService,
+  ) {}
 
   public Tab;
 
   ngOnInit(): void {
-    
+    this.userData = this.authservice.userData();
+    this.getLabels();
+  }
+
+  getLabels(){
+    let payload = {
+      "userName" : this.userData.userName,
+      "wsid": this.userData.wsid
+    }
+    this.locationService.get(payload,'/Admin/GetTransactionTypeCounts').subscribe((res =>{
+      if (res.isExecuted && res.data) {
+        res.data.forEach(item => {
+          if (item.transactionType === "Count") {
+            this.countLabel = `Count (${item.count})`;
+          } else if (item.transactionType === "Pick") {
+            this.pickLabel = `Pick (${item.count})`;
+          } else if (item.transactionType === "Put Away") {
+            this.putAwayLabel = `Put Away (${item.count})`;
+          }
+        });
+      } 
+    }));
   }
 
   ngAfterViewInit(){
