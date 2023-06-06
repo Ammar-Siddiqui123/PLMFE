ee2a582e29b12398a3132b93dc37741ed8d9bf7e
Umer Aslam <umer.aslam@enterprise64.com>
2023-06-06 14:30:04 +0500

    wip

--- src/app/admin/move-items/move-items.component.ts
+++ src/app/admin/move-items/move-items.component.ts
@@ -52,8 +52,12 @@ const TRNSC_DATA = [
 export class MoveItemsComponent implements OnInit {
   floatLabelControl = new FormControl('auto' as FloatLabelType);
   public dataSource: any = new MatTableDataSource();
+  public moveToDatasource: any = new MatTableDataSource();
+
   userData: any;
   itemNo: any = '';
+ 
+  
   reqDate: Date = new Date();
   sortOrder = 'asc';
   sortCol = 0;
@@ -62,10 +66,24 @@ export class MoveItemsComponent implements OnInit {
   endRow = 10;
   recordsPerPage = 10;
   recordsFiltered = 0;
+
+  sortOrderTo = 'asc';
+  sortColTo = 0;
+  totalRecordsTo = 0;
+  startRowTo = 0;
+  endRowTo = 10;
+  recordsPerPageTo = 10;
+  recordsFilteredTo = 0;
+
   viewMode = 'NOA';
   viewModeTo = 'NOA';
-  viewAll=false
+
+  invMapID=-1;
+  invMapIDToItem=-1;
+
+  viewAll = false;
   customLabel = '';
+  customLabelTo = '';
   from_priority = 0;
   from_warehouse = '';
   from_location = '';
@@ -85,6 +103,7 @@ export class MoveItemsComponent implements OnInit {
   MoveFromDedicated = '';
   MoveToDedicated = '';
   pageEvent: PageEvent;
+  pageEventTo: PageEvent;
   itemNumberSearch = new Subject<string>();
   hideRequiredControl = new FormControl(false);
   searchAutocompletItemNo: any = [];
@@ -139,40 +158,58 @@ export class MoveItemsComponent implements OnInit {
   ];
   stageTable: any = [];
   columnSeq: any = [];
-  getMoveItemList(tableName) {
+  getMoveItemList(tableName,fromPagination=false) {
+    if (tableName === 'MoveTo') {
 
-    if(tableName==='MoveTo' &&  (this.viewAll || this.dataSource.data.length===0 )){
-      this.viewModeTo='All'
-    }else{
-      this.viewModeTo='NOA'
+      if (this.viewAll || this.dataSource.data.length === 0) {
+        this.viewModeTo = 'All';
+      } else if(fromPagination){
+        this.viewModeTo = 'All';
+      }else {
+        this.viewModeTo = 'NOA';
+      }
     }
-    
+
     let payload = {
       draw: 1,
-      sRow: this.startRow,
-      eRow: this.endRow,
-      searchString: this.itemNo,
+      sRow: tableName === 'MoveFrom' ?this.startRow:this.startRowTo,
+      eRow: tableName === 'MoveFrom' ?this.endRow:this.endRowTo,
+      searchString: tableName === 'MoveFrom' ? this.itemNo:this.from_itemNo,
       searchColumn: 'Item Number',
-      sortColumnIndex: this.sortCol,
-      sortOrder: this.sortOrder,
+      sortColumnIndex: tableName === 'MoveFrom' ? this.sortCol:this.sortColTo,
+      sortOrder:  tableName === 'MoveFrom' ? this.sortOrder :this.sortOrderTo,
       username: this.userData.userName,
       tableName: tableName,
       cellSize: this.from_cellSize,
       warehouse: this.from_warehouse,
-      invMapid: -1,
-      viewMode: tableName==='MoveFrom'?this.viewMode:this.viewModeTo,
+      invMapid: tableName === 'MoveFrom' ? this.invMapID:this.invMapIDToItem,
+      viewMode: tableName === 'MoveFrom' ? this.viewMode : this.viewModeTo,
       filter: '1 = 1',
       wsid: this.userData.wsid,
     };
     this.adminService
       .get(payload, '/Admin/GetMoveItemsTable')
       .subscribe((res: any) => {
-        this.dataSource = new MatTableDataSource(res?.data['moveMapItems']);
-        this.totalRecords = res?.data.recordsTotal;
-        this.recordsFiltered = res?.data.recordsFiltered;
-        this.customLabel = `Showing page ${this.totalRecords} of ${Math.ceil(
-          this.totalRecords / this.recordsPerPage
-        )}`;
+        if (tableName === 'MoveTo') {
+          this.moveToDatasource = new MatTableDataSource(
+            res?.data['moveMapItems']
+          );
+          this.totalRecordsTo=res?.data.recordsTotal;
+          this.recordsFilteredTo = res?.data.recordsFiltered;
+          this.customLabelTo = `Showing page ${this.totalRecords} of ${Math.ceil(
+            this.totalRecords / this.recordsPerPage
+          )}`;
+        } else {
+          this.dataSource = new MatTableDataSource(res?.data['moveMapItems']);
+          this.totalRecords = res?.data.recordsTotal;
+          this.recordsFiltered = res?.data.recordsFiltered;
+          this.customLabel = `Showing page ${this.totalRecords} of ${Math.ceil(
+            this.totalRecords / this.recordsPerPage
+          )}`;
+        }
+
+    
+     
 
         // this.displayedColumns = TRNSC_DATA;
       });
@@ -220,6 +257,26 @@ export class MoveItemsComponent implements OnInit {
     this.sortOrder = event.direction;
     this.getMoveItemList('MoveFrom');
   }
+
+  sortChangeToItems(event) {
+    if (
+      !this.moveToDatasource._data._value ||
+      event.direction == '' ||
+      event.direction == this.sortOrderTo
+    )
+      return;
+
+    let index;
+    this.displayedColumns.find((x, i) => {
+      if (x === event.active) {
+        index = i;
+      }
+    });
+
+    this.sortColTo = index;
+    this.sortOrderTo = event.direction;
+    this.getMoveItemList('MoveTo');
+  }
   handlePageEvent(e: PageEvent) {
     this.pageEvent = e;
     // this.customPagination.startIndex =  e.pageIndex
@@ -233,7 +290,21 @@ export class MoveItemsComponent implements OnInit {
     // this.initializeApi();
     this.getMoveItemList('MoveFrom');
   }
+  handlePageEventTo(e: PageEvent) {
+    this.pageEventTo = e;
+    // this.customPagination.startIndex =  e.pageIndex
+    this.startRowTo = e.pageSize * e.pageIndex;
+
+    this.endRowTo = e.pageSize * e.pageIndex + e.pageSize;
+    // this.length = e.length;
+    this.recordsPerPageTo = e.pageSize;
+    // this.pageIndex = e.pageIndex;
+
+    // this.initializeApi();
+    this.getMoveItemList('MoveTo',true);
+  }
   getMoveFromDetails(row) {
+    this.invMapIDToItem=row.invMapID;
     this.from_warehouse = row.warehouse;
     this.from_location = row.location;
     this.from_itemNo = row.itemNumber;
@@ -248,7 +319,7 @@ export class MoveItemsComponent implements OnInit {
     this.isDedicated = row.dedicated === true ? true : false;
     this.fillQty =
       row.itemQuantity - row.maximumQuantity - row.quantityAllocatedPutAway;
-
+   
     if (this.fillQty < 0) {
       this.fillQty = 0;
     }
@@ -261,9 +332,7 @@ export class MoveItemsComponent implements OnInit {
       this.from_itemQuantity = this.maxMoveQty;
     }
 
-
-
-    this.getMoveItemList('MoveTo')
+    this.getMoveItemList('MoveTo');
   }
 
   openAlertDialog(type, maxMoveQty?) {
