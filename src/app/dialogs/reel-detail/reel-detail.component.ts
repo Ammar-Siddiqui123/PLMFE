0e7a94865f512e003090237598bea2506baf6c64
FuzailAhmedE64 <fuzail.ahmed@enterprise64.com>
2023-06-28 15:52:32 +0500

    Responsiveness-Fuzail
    Reel details & Reel Transactions popups

---
+++ src/app/dialogs/reel-detail/reel-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reel-detail',
+  templateUrl: './reel-detail.component.html',
+  styleUrls: ['./reel-detail.component.scss']
+})
+export class ReelDetailComponent implements OnInit {
+
+
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
