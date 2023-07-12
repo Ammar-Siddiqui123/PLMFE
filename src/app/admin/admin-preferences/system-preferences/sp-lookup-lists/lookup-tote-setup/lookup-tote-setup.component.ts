59b25edcc2f4c749b41895829ee4335d1575334f
FuzailAhmedE64 <fuzail.ahmed@enterprise64.com>
2023-07-12 18:36:21 +0500

    -- Lookup UI 4 Tabs
    4 Tabs

---
+++ src/app/admin/admin-preferences/system-preferences/sp-lookup-lists/lookup-tote-setup/lookup-tote-setup.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lookup-tote-setup',
+  templateUrl: './lookup-tote-setup.component.html',
+  styleUrls: ['./lookup-tote-setup.component.scss']
+})
+export class LookupToteSetupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
