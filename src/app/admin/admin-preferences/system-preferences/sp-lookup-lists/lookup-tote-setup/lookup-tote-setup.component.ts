4220adf0f5b5647d20d4714980af93221e9dc8ad
FuzailAhmedE64 <fuzail.ahmed@enterprise64.com>
2023-07-13 14:29:00 +0500

    Tote Setup Screen UI
    Tote Setup Screen

--- src/app/admin/admin-preferences/system-preferences/sp-lookup-lists/lookup-tote-setup/lookup-tote-setup.component.ts
+++ src/app/admin/admin-preferences/system-preferences/sp-lookup-lists/lookup-tote-setup/lookup-tote-setup.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LookupToteSetupComponent implements OnInit {
 
+
+  ELEMENT_DATA: any[] =[
+    {tote_id: '125874', cells: '120' },
+  ];
+
+  displayedColumns: string[] = ['tote_id', 'cells', 'actions'];
+  tableData = this.ELEMENT_DATA
+  dataSourceList:any
+
   constructor() { }
 
   ngOnInit(): void {
