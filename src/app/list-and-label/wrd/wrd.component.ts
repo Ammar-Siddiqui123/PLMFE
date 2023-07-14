20eb6e4db84aebe790e53cdcace2a5d9adcb419d
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-07-14 18:30:18 +0500

    add env list and label

--- src/app/list-and-label/wrd/wrd.component.ts
+++ src/app/list-and-label/wrd/wrd.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Route, Router } from 'angular-routing';
 import { SharedService } from 'src/app/services/shared.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-wrd',
@@ -7,10 +10,29 @@ import { SharedService } from 'src/app/services/shared.service';
   styleUrls: ['./wrd.component.scss']
 })
 export class WrdComponent implements OnInit {
-
-  constructor() { }
-
+  env:string;
+  @ViewChild('ListAndLabel', { read: ViewContainerRef }) ListAndLabel: ViewContainerRef;
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+     
+    this.env = location.protocol + '//' + location.host; 
+ 
+   }
   ngOnInit(): void {
+    setTimeout(() => {
+      this.generateHTMLAndAppend() ;
+      
+    }, 250);
   }
-
+  generateHTMLAndAppend() { 
+    const dynamicHtml = `
+    <iframe style="width: 100%; height: 1000px;" id="wrdFrame" src="${this.env}/#/ListAndLabel/report">
+    </iframe>
+    `; 
+    const dynamicComponent = Component({
+      template: dynamicHtml
+    })(class {}); 
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(dynamicComponent); 
+    this.ListAndLabel.clear(); 
+    const componentRef = this.ListAndLabel.createComponent(componentFactory);
+  }
 }
--- src/app/list-and-label/wrv/wrv.component.ts
+++ src/app/list-and-label/wrv/wrv.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-wrv',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./wrv.component.scss']
 })
 export class WrvComponent implements OnInit {
-
-  constructor() { }
-
+  env:string;
+  @ViewChild('ListAndLabel', { read: ViewContainerRef }) ListAndLabel: ViewContainerRef;
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+     
+    this.env = location.protocol + '//' + location.host; 
+ 
+   }
   ngOnInit(): void {
+    setTimeout(() => {
+      // this.generateHTMLAndAppend() ;
+      
+    }, 250);
   }
-
+  generateHTMLAndAppend() { 
+    const dynamicHtml = `
+    <iframe style="width: 100%; height: 1000px;" id="wrdFrame" src="${this.env}/#/ListAndLabel/report-view">
+    </iframe>
+    `; 
+    const dynamicComponent = Component({
+      template: dynamicHtml
+    })(class {}); 
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory(dynamicComponent); 
+    this.ListAndLabel.clear(); 
+    const componentRef = this.ListAndLabel.createComponent(componentFactory);
+  }
 }
