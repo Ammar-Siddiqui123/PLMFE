f2bb6fa6c150f3d2b532a5c49d43076130fc27fb
Umer Aslam <umer.aslam@enterprise64.com>
2023-07-25 10:20:04 +0500

    further add field mapping impact

--- src/app/admin/dialogs/batch-manager-detail-view/batch-manager-detail-view.component.ts
+++ src/app/admin/dialogs/batch-manager-detail-view/batch-manager-detail-view.component.ts
@@ -3,6 +3,7 @@ import { Component, Inject, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { ApiFuntions } from 'src/app/services/ApiFuntions';
 
 @Component({
   selector: 'app-batch-manager-detail-view',
@@ -11,22 +12,30 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class BatchManagerDetailViewComponent implements OnInit {
 
-
+  fieldNames:any;
   displayedColumns: string[] = ['item_no', 'description','transaction_qty','lotNo', 'expiration_date', 'uom', 'serial_no', 'notes','location','warehouse','userField1','userField2','toteID'];
   dataSource:any = [];
   @ViewChild(MatSort, { static: true }) sort: MatSort;
-  constructor(private _liveAnnouncer: LiveAnnouncer,@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<any>) { 
+  constructor(private Api:ApiFuntions, private _liveAnnouncer: LiveAnnouncer,@Inject(MAT_DIALOG_DATA) public data: any, public dialogRef: MatDialogRef<any>) { 
 
 
     
   }
 
   ngOnInit(): void {
+    this.OSFieldFilterNames();
     this.dataSource = new MatTableDataSource(this.data);
     // this.dataSource.sort = this.sort
   }
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+  }
+
+  public OSFieldFilterNames() { 
+    this.Api.ColumnAlias().subscribe((res: any) => {
+      this.fieldNames = res.data;
+   
+    })
   }
   announceSortChange(sortState: Sort) {
     if (sortState.direction) {
