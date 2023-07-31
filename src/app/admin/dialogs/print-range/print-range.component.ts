99bb742775e7a6c2a09ef66638ebc58c24369c19
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-07-31 11:04:01 +0500

    inventory map print functionality

--- src/app/admin/dialogs/print-range/print-range.component.ts
+++ src/app/admin/dialogs/print-range/print-range.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/init/auth.service';
 
 @Component({
   selector: 'app-print-range',
@@ -9,12 +10,19 @@ import { Router } from '@angular/router';
 })
 export class PrintRangeComponent implements OnInit {
   @ViewChild('begin_loc') begin_loc: ElementRef;
+  userData:any;
+  beginLoc:string = "";
+  endLoc:string = "";
+  groupLikeLoc:boolean = false;
+  quantitySelected:number = 0;
   constructor(
     private route: Router,
     public dialogRef: MatDialogRef<any>,
+    private authService: AuthService,
   ) { }
 
   ngOnInit(): void {
+    this.userData = this.authService.userData();
   }
   
   ngAfterViewInit() {
@@ -23,6 +31,6 @@ export class PrintRangeComponent implements OnInit {
 
   printRange(){
     this.dialogRef.close();
-    this.route.navigateByUrl(`/report-view?file=LocLabel-lbl`);
+    this.route.navigateByUrl(`/report-view?file=FileName:printIMReport|invMapID:0|groupLikeLoc:${this.groupLikeLoc}|beginLoc:${this.beginLoc}|endLoc:${this.endLoc}|User:${this.userData.userName}`);
   }
 }
