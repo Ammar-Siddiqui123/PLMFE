51603ce50d76f4e70091b7d2f9b2342d9af8261e
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-08-02 20:17:29 +0500

    fixes

--- src/app/admin/transaction/open-transaction/open-transaction-on-hold/open-transaction-on-hold.component.ts
+++ src/app/admin/transaction/open-transaction/open-transaction-on-hold/open-transaction-on-hold.component.ts
@@ -825,12 +825,12 @@ this.router.navigate([]).then((result) => {
   }
 
   printCycleCountReport(){
-    window.location.href = `/report-view?file=FileName:printCycleCountReport`
+    window.location.href = `/#/report-view?file=FileName:printCycleCountReport`
     // this.router.navigateByUrl(`/report-view?file=FileName:printCycleCountReport`);
   }
 
   previewFiftyPagesOnly(){
-    window.location.href = `/report-view?file=CycleCount-lst`
+    window.location.href = `/#/report-view?file=CycleCount-lst-prv`;
     // this.router.navigateByUrl(`/report-view?file=CycleCount-lst`);
   }
 }
