d67dbf366c1baa14a95be549b2ae061890f775a7
uzairsaqib786 <100584002+uzairsaqib786@users.noreply.github.com>
2023-08-11 00:18:04 +0500

    RE RE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   apiUrl: "https://staging-e64.com:9012/api",
   // apiUrl: "http://localhost:63590/api",  
   userName: "pickpro",
-  /////////////////////////////////////////
+  ///////////////////////////////////////
   password: "Admin$11"
 };
 
