44a214372654ec06a620123700ff2081d9f902bd
Hamza-Ent64 <100584002+uzairsaqib786@users.noreply.github.com>
2023-09-25 12:29:42 +0500

    Assignment 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, HostListener, OnInit, VERSION } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-import { filter, map } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
 import { BrowserCloseService } from './services/browser-close.service';
 import { ApiFuntions } from './services/ApiFuntions';
 import { BroadcastService } from './init/broadcast.service';
@@ -9,7 +8,7 @@ import { BroadcastService } from './init/broadcast.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: [],
 })
 export class AppComponent implements OnInit {
   userData;
@@ -29,16 +28,9 @@ beforeunloadHandler(event): void {
 }
 @HostListener('window:load',['$event'])
 onPageLoad(event: Event) {
-   let loadTime = new Date();
-  let unloadTime:any = localStorage.getItem('unloadTime');
-  this.userData = JSON.parse(localStorage.getItem('user') || '{}');
-  if (unloadTime) {
-    unloadTime = new Date(JSON.parse(unloadTime));
-    let refreshTime = loadTime.getTime() - unloadTime.getTime();
 
-    if (refreshTime > 3000) {
-    }
-  }
+  this.userData = JSON.parse(localStorage.getItem('user') ?? '{}');
+
 }
   ngOnInit() {
 
